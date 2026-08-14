08e705a617d8bdf2307159d9b8146d1451678653
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 17:19:55 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/quality-assurance-governance.php
@@ -0,0 +1,207 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">Quality assurance governance&ZeroWidthSpace;</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">Get ahead of the game in quality assurance with PRESTO P-D-C-A</h2>
+                    
+                                            <div class="content">A toolkit for you on quality!</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/quality_mgmt.webp" alt="quality assurance governance">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+
+<div
+   class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+   <div class="row">
+      <div class="col-sm-12">
+         <h4 class="title text-center">Download Your Free Resource</h4>
+
+         <div
+            class="wpcf7 js"
+            id="wpcf7-f2784-o1"
+            lang="en-US"
+            dir="ltr"
+            data-wpcf7-id="2784"
+         >
+            <div class="screen-reader-response">
+               <p role="status" aria-live="polite" aria-atomic="true"></p>
+               <ul></ul>
+            </div>
+            <form
+               action="/resources/quality-assurance-governance/#wpcf7-f2784-o1"
+               method="post"
+               class="wpcf7-form init"
+               aria-label="Contact form"
+               novalidate="novalidate"
+               data-status="init"
+               data-hs-cf-bound="true"
+            >
+               <fieldset class="hidden-fields-container">
+                  <input type="hidden" name="_wpcf7" value="2784" /><input
+                     type="hidden"
+                     name="_wpcf7_version"
+                     value="6.1"
+                  /><input
+                     type="hidden"
+                     name="_wpcf7_locale"
+                     value="en_US"
+                  /><input
+                     type="hidden"
+                     name="_wpcf7_unit_tag"
+                     value="wpcf7-f2784-o1"
+                  /><input
+                     type="hidden"
+                     name="_wpcf7_container_post"
+                     value="0"
+                  /><input
+                     type="hidden"
+                     name="_wpcf7_posted_data_hash"
+                     value=""
+                  /><input
+                     type="hidden"
+                     name="_wpcf7_recaptcha_response"
+                     value="0cAFcWeA5JBqnE9rquc5NjEstiIqxU1FYRdAqhwi4uH99VRqQT58oY1sYM3E7Z4bmU3zy1f1y9vHdZdY7NqMPVtQ8CP-OzIOuFulCKlbhHV8jH_HSXW08UpKfIEv52_vIFdkzhK51tD8RlwRinYQjeaj73Vh5fha5w0L2cE4MfiDA9xmgnlnA6_68SdP6upozlyu9j8w9t2Q8VFWmbuFE0Dy9cCgiOrKvRmlY501DI12yuIVbJhTBW0qqa3EahVWNq1kv4mUmKpwfrb3Ur8mvQy0wACB1AzF20rHgBn3eC6p3jPTVVhmfpXFvzhPPKMKkPNTcly3Wq_fW5Miv5SsltokFcEwXAmusEFM-OmEyfHNTjAs1_8LrB8hlUG7cVxLsw0BCd6B2AMh6elJRlACWqUlc6FvbmaqmMQezBRgSvcxTlbdFZ5z-JXmdC3_P043usedpg2EY-I3Pi6EvRAB4tJXJ_qKJpHEc4M1p40b7Rg5ae04rIbQ3sx-9flK3dXom3BXZTvQPvrlxfpgiIEJAsx_kTR3LRpE7B-iI9uOApHL8tz1B8qbPQfnIMGnRKlrQcpAsys1931eH0PDXNcsCXCMJ2HXTkRxOmgrToOCVyLoQof2Wl8F7VMTJNxjpgDIVTZbNstzZr6SuJF1CjvsMyXPkK57JCqc03-6916NWPcv5MyYTWJpAeihqI-6P-voZJ5KTlJPkOOpzoMpIy6jWn11KmmPox2cMSWzVx2_fpaLKNqrglLkXQrOkcXM_4svk_9LuWMP6Fzplt66HFHTyD6Prn3ThSbmUi6BZpAx-9qn-yeXP3QA0_OUPnXm5UqAYVUro3S5xjpXzYgJjd8wENQOa9IFu5Btqd6a6_bvJw99zIxReJ9uMyvV4p3x0xGXkRHXxBkQPQYLIsNyhYWlhMJumEcbSfujQkAIs3tYE2KtQK4BwBecEI3DV08JG4fWf5kG6_sZ5XjTOLTb7ngTAZFSNUxUeaTocXc4hwEBKcoVpf90_ZffOs501DxdMdHRCy6J-8i39-nyHCsf3AhhRMiz_h1Db4TEno1d92SmJdSLRjDgzY9MRTtC1IgTIlLH69kbvCI2Sc9Blch5najJLUHex6fY7ARaUPLT2vYa3G0pGHtbARl3JDIakQfSqeLI2giuDyAefeCNOh8rYsJeF-CtoyM1p9rseuZRWg9uDJ4yYJ8UQL-xNQCFScRUheXmwTT5HHfnu5VNlS9n2dWlP_o5Un8krDdyhfqhQI2vcDax6JN_AB7GevSZr1LBtFYygKrbLUelqTRbin-8tjwQ-GmQ82gaVG2vhXbMWkOHUI9En8Uf50GRK5ZHuxYfCPjm6KD84TDYGbiS6RPl6twJxF_BZ1Xl7OEySqp0cN4nKlZCkvSjMg1B2FPpnN0Fm9Q4UvQt81FzvH1EPkmCddtfVwt2186Ng9SOp8LUVlkGYC9v1TER0QLIk5G2-ru0aCm1TV-ithQ_Nbf7Vs20nkToCUy89Fk-k-kyNFK3kYXt4EyuxhBCZcfwyHiIccPmfC7f2GFzvQSDWMkjTRilahg8VJ82QgB6pD1G18gNdUWYT7441rExrnrPYxfpTXpKA0xZf40oPb4jx5Fk1mCK9RES49gQun0t0LD7M_4ZwY7xCjkPp_9kpmTbRcil3CMaNDTMBwK-8S4hwHBl4b0dXROzmCMUdRwz7rTB9QL8LCeMIPROqxS_GtAKvGKdSBUh8mdrZ61GBMfYVGc7Inglz0W9D0R7IxXUtAjtwN_SkTNEw6OUyJSCv-DZTcmveZsAq-CsuPqewGp3_fDzK1QQVfHBu1FoRLfwb2_CdUHuSEZL-fo0ENbilyhQYxEGFg7wsXWm127-X6zvbuLEpeqJeJMWVhrsy3oP1DaTxiBkb7oTiH9oIaLKDGtWtdgUu1_-j3dw9ERC54HMfN_ZIgQjDqu4yEhfRhCfkVs8-DsJaE-KIJmk867PtVeDBv_vlRxk1sHzXz5LcOWQQWoRh6DIQ_dYfNsy_rl06evIU6wCN4Kbf08JbnskH-3utJv6gW6Qj1eoOWLmUUJ0ik9fuLnXWZRdc7tkgHIfn1-yAGG7H2oYDlgplyGDoWCh5HnmAbGxAbl_LdTCmc8serrjinMXxIydktjOtl2CtQDvZTFew_GFLSIm_iQ2KNAjUsaSCPf5VJ3FKtMn9a2ogVoleIKceBkKnNIR3-eUfdoj117e-I1SAeQy_iLux6GUvvukMJWoI0EYxiZjEWt5c3sFLB-Kjxmx3-td_W51BTVTw3X2WFaD6wg7cXJF32hxAi6IuSLAV-CBIoSIIU9GftlkwO"
+                  />
+               </fieldset>
+               <p>
+                  <label>
+                     Your name<br />
+                     <span class="wpcf7-form-control-wrap" data-name="your-name"
+                        ><input
+                           size="40"
+                           maxlength="400"
+                           class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                           autocomplete="name"
+                           aria-required="true"
+                           aria-invalid="false"
+                           value=""
+                           type="text"
+                           name="your-name"
+                     /></span>
+                  </label>
+               </p>
+               <p>
+                  <label>
+                     Your email<br />
+                     <span
+                        class="wpcf7-form-control-wrap"
+                        data-name="your-email"
+                        ><input
+                           size="40"
+                           maxlength="400"
+                           class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                           autocomplete="email"
+                           aria-required="true"
+                           aria-invalid="false"
+                           value=""
+                           type="email"
+                           name="your-email"
+                     /></span>
+                  </label>
+               </p>
+               <p>
+                  <input
+                     class="wpcf7-form-control wpcf7-submit has-spinner"
+                     type="submit"
+                     value="Submit"
+                  /><span class="wpcf7-spinner"></span>
+               </p>
+               <div class="wpcf7-response-output" aria-hidden="true"></div>
+            </form>
+         </div>
+      </div>
+   </div>
+</div>
+
+<section class="field-guide">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-10 offset-lg-1">
+            <div class="section-title element-space-20 text-center">
+               <h2 class="title">
+                  Quality Management Program Is? Take this 6-part self
+                  assessment now
+               </h2>
+
+               <div class="resource-bottom-desc element-space-20">
+                  <p>
+                     Catalogue business processes, map inputs and outputs and
+                     start capturing waste in record time. Create your
+                     organisation, add personnel, products and market segments
+                     and OFF YOU GO. Schedule and conduct audits, capture
+                     observations and follow through on corrective actions to
+                     ensure quality control is embedded in your company’s DNA.
+                  </p>
+                  <p>
+                     PRESTO P-D-C-A with its rich set of out-of-the-box reports
+                     created together with a team of Swiss French engineers will
+                     help you avoid 6 critical mistakes the majority of quality
+                     managers in the market are making.
+                  </p>
+                  <p>
+                     FIND OUT NOW what these 6 mistakes are and blast off to
+                     your next destination in digital transformation excellence.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="get-started bg-light-gray position-relative">
+   <div class="container">
+      <div class="row row-gap-5 align-items-center">
+         <div class="col-md-7 mb-5 mb-md-0">
+            <div class="started__content">
+               <h2>Get Started in Seconds</h2>
+               <div class="btn-wrap justify-content-start">
+                  <a
+                     href="https://www.prestopdca.com/contact-us/"
+                     class="btn btn-fill btn-icon icon-white"
+                     >Book a Demo</a
+                  >
+                  <a
+                     href="https://www.prestopdca.com/pricing/"
+                     class="btn btn-icon icon-black"
+                     >Get Started</a
+                  >
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="started__img">
+               <div class="media">
+                  <img
+                     src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp"
+                     alt="Get Started Image"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
