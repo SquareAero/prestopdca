29681d19ba499371ede29ec2ade0993bda659999
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 16:08:58 +0530

    Add files via upload

---
+++ Learn/Case Studies/single-case/presto-pdca-kpi-manager-used-to-digitize-performance-management-and-enrich-analytics-based-decision-making-at-union-paper-mills-of-dubai.php
@@ -0,0 +1,306 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+<section class="case-study-detail padding">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8 col-md-6 mb-4 mb-md-0">
+            <h1 class="title">
+               Presto PDCA KPI manager used to digitize performance management
+               and enrich analytics-based decision making at Union Paper Mills
+               of Dubai
+            </h1>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="company__info element-space-30">
+               <div class="company__logo">
+                  <div class="media">
+                     <img
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/upm-1.png"
+                        alt="Company Logo"
+                     />
+                  </div>
+               </div>
+
+               <div class="feature__list element-space-20">
+                  <div class="list__item">
+                     <h3>Industry</h3>
+                     <p>Paper Manufacturing</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Company size</h3>
+                     <p>Mid-market</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Teams</h3>
+                     <p>Company-wide</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Products used</h3>
+                     <p>KPI Reporting &amp; Issue Management</p>
+                  </div>
+               </div>
+
+               <div class="btn-wrap justify-content-start">
+                  <a href="#" class="btn btn-fill" target=""> Enterprise </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="case-study-content">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="left-sidebar">
+               <div class="sidebar-list">
+                  <h4 class="sub-title">In this story</h4>
+                  <hr class="divider" />
+                  <ul>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#from-spreadsheets-to-real-time-cloud-based-performance-management"
+                           >From spreadsheets to real-time, cloud-based
+                           performance management</a
+                        >
+                     </li>
+                     <li>
+                        <a class="side-link" href="#core-objectives"
+                           >Core Objectives</a
+                        >
+                     </li>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#case-study-details-download-here"
+                           >Case Study Details Download Here</a
+                        >
+                     </li>
+                  </ul>
+                  <div class="btn-wrap justify-content-center mt-5">
+                     <a href="#" class="btn btn-fill btn-icon icon-white"
+                        >Book a Demo</a
+                     >
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-8">
+            <div class="case-study-description">
+               <div class="case-author_bio">
+                  <div class="author__top flex-wrap align-items-center">
+                     <div class="author__img mb-3 mb-lg-0">
+                        <img
+                           src="https://www.prestopdca.com/wp-content/uploads/2026/05/shabbir-haideri.jpeg"
+                           alt="Shabbir Haideri"
+                        />
+                     </div>
+                     <div class="author__text ps-0">
+                        <div class="author__name">Shabbir Haideri</div>
+                        <div class="author__designation">General Manager</div>
+                     </div>
+                  </div>
+                  <div class="author__desc">
+                     "With PRESTO we became a more flexible organisation
+                     offering our staff and their leaders a structured means to
+                     capture know-how, share best practices and collaborate to
+                     solve daily challenges"
+                  </div>
+               </div>
+               <div class="post-full__content case-summary element-space-30">
+                  <h2
+                     id="from-spreadsheets-to-real-time-cloud-based-performance-management"
+                  >
+                     From spreadsheets to real-time, cloud-based performance
+                     management
+                  </h2>
+                  <p>
+                     “RECOVER, RECYCLE, RECREATE” – Collection of Waste Paper by
+                     Union Paper Mills keeps UAE’s environment neat and clean
+                     adding value to waste. Thus, UPM supports a world with an
+                     industry wherein paper production is less reliant on virgin
+                     fibre and forest products. By the use of the latest
+                     tecnology in environmental friendly manufacturing, UPM is
+                     known throughout the UAE as a pioneer in recycled waste
+                     paper to produce containerboard, benefiting the country in
+                     particular and the world at large in terms of reduction in
+                     carbon footprints.
+                  </p>
+                  <p>
+                     Having digital transformation on the top of its agenda and
+                     looking for a more effective way to review prior-day shop
+                     floor performance statistics and analytics in the daily
+                     morning management meeting, UPM chose PRESTO KPI as its KPI
+                     management solution to accelerate tactical decision making
+                     and remove the risks and administrative burden of the
+                     offline solutions (spreadsheets) previously used to manage
+                     80+ daily performance indicators
+                  </p>
+                  <h3 id="core-objectives" class="wp-block-heading">
+                     Core Objectives
+                  </h3>
+                  <ol class="wp-block-list">
+                     <li>
+                        Enhance sense of urgency in the management of
+                        underperforming KPIs
+                     </li>
+                     <li>
+                        Increase sense of ownership and sense of purpose at the
+                        subject matter level
+                     </li>
+                     <li>
+                        Create a non-confrontational continuous improvement
+                        culture where issues, problems, and their related wastes
+                        are visible
+                     </li>
+                     <li>
+                        Reduce time dedicated by stakeholders to excessive
+                        documentation, paperwork and communication
+                     </li>
+                     <li>
+                        Solve problems at the seed level and attack waste by
+                        taking action before the problem becomes chronic
+                     </li>
+                     <li>
+                        Automate UPM’s KPI management process to reduce risk and
+                        increase operational efficiency
+                     </li>
+                  </ol>
+                  <h2
+                     id="case-study-details-download-here"
+                     class="wp-block-heading"
+                  >
+                     <a
+                        href="https://drive.google.com/file/d/1m57YI7Lq5tygIlal-fz1bicwWFJJJ6Y1/view?usp=sharing"
+                        data-type="link"
+                        >Case Study Details Download Here</a
+                     >
+                  </h2>
+                  <p>&nbsp;</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="pesto-products padding">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6 offset-lg-3">
+            <div class="section-title text-center">
+               <h2 class="title blue-gradient-ltr clip">Results to date</h2>
+            </div>
+         </div>
+      </div>
+      <div class="products">
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/minutes.png"
+                  alt="25 Hours (cumulative)"
+               />
+            </div>
+            <div class="product__content">
+               <h4>25 Hours (cumulative)</h4>
+               <p>hours per week saved on administrative activities</p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/Functional.svg"
+                  alt="642 Staff"
+               />
+            </div>
+            <div class="product__content">
+               <h4>642 Staff</h4>
+               <p>were assigned roles and responsibilities</p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/350-KPIs.svg"
+                  alt="920 Corrective Actions"
+               />
+            </div>
+            <div class="product__content">
+               <h4>920 Corrective Actions</h4>
+               <p>agreed and assigned across business operations</p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/10-reports.svg"
+                  alt="158 CI Opportunities"
+               />
+            </div>
+            <div class="product__content">
+               <h4>158 CI Opportunities</h4>
+               <p>
+                  approved and assigned to team leaders &amp; middle management
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/24-hours.svg"
+                  alt="60 Minutes"
+               />
+            </div>
+            <div class="product__content">
+               <h4>60 Minutes</h4>
+               <p>
+                  was all that was needed per staff member to start using Presto
+                  successfully
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="get-started bg-light-gray position-relative">
+   <div class="container">
+      <div class="row row-gap-5 align-items-center">
+         <div class="col-md-7 mb-5 mb-md-0">
+            <div class="started__content">
+               <h2>Get Started in Seconds</h2>
+               <div class="btn-wrap justify-content-start">
+                  <a
+                     href="https://www.prestopdca.com/contact-us/"
+                     class="btn btn-fill btn-icon icon-white"
+                     >Book a Demo</a
+                  >
+                  <a
+                     href="https://www.prestopdca.com/pricing/"
+                     class="btn btn-icon icon-black"
+                     >Get Started</a
+                  >
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="started__img">
+               <div class="media">
+                  <img
+                     src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp"
+                     alt="Get Started Image"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
