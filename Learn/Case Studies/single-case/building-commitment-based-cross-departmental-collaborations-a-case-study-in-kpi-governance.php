8c52ebcbcbd09995b21376c8bce1ad182bda100d
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 16:44:22 +0530

    Add files via upload

---
+++ Learn/Case Studies/single-case/building-commitment-based-cross-departmental-collaborations-a-case-study-in-kpi-governance.php
@@ -0,0 +1,342 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section class="case-study-detail padding">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8 col-md-6 mb-4 mb-md-0">
+            <h1 class="title">
+               Building Commitment-Based, Cross-Departmental Collaborations.A
+               Case Study In KPI Governance
+            </h1>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="company__info element-space-30">
+               <div class="company__logo">
+                  <div class="media">
+                     <img
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/alvis.png"
+                        alt="Company Logo"
+                     />
+                  </div>
+               </div>
+
+               <div class="feature__list element-space-20">
+                  <div class="list__item">
+                     <h3>Industry</h3>
+                     <p>Healthcare</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Company size</h3>
+                     <p>Mid-market</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Teams</h3>
+                     <p>Company-wide</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Products used</h3>
+                     <p>Products used</p>
+                  </div>
+               </div>
+
+               <div class="btn-wrap justify-content-start">
+                  <a href="#" class="btn btn-fill" target=""> Enterprise </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="case-study-content">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="left-sidebar">
+               <div class="sidebar-list">
+                  <h4 class="sub-title">In this story</h4>
+                  <hr class="divider" />
+                  <ul>
+                     <li>
+                        <a class="side-link" href="#building-commitment-based"
+                           >Building Commitment-Based</a
+                        >
+                     </li>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#cross-departmental-collaborations"
+                           >Cross-Departmental Collaborations</a
+                        >
+                     </li>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#a-case-study-in-kpi-governance"
+                           >A Case Study In KPI Governance</a
+                        >
+                     </li>
+                  </ul>
+                  <div class="btn-wrap justify-content-center mt-5">
+                     <a href="#" class="btn btn-fill btn-icon icon-white"
+                        >Book a Demo</a
+                     >
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-8">
+            <div class="case-study-description">
+               <div class="case-author_bio">
+                  <div class="author__top flex-wrap align-items-center">
+                     <div class="author__img mb-3 mb-lg-0">
+                        <img
+                           src="https://www.prestopdca.com/wp-content/uploads/2025/07/kattie-1.png"
+                           alt="Kathleen Geyer"
+                        />
+                     </div>
+                     <div class="author__text ps-0">
+                        <div class="author__name">Kathleen Geyer</div>
+                        <div class="author__designation">
+                           Data Analyst &amp; Research Coordinator
+                        </div>
+                     </div>
+                  </div>
+                  <div class="author__desc">
+                     "PRESTO offers a quick and easy way to stay on top of all
+                     my cross-departmental relationships giving me the
+                     non-confrontational reporting package I need to propose
+                     improvement opportunities in service levels &amp;
+                     interactions"
+                  </div>
+               </div>
+               <div class="post-full__content case-summary element-space-30">
+                  <h3 id="building-commitment-based" class="wp-block-heading">
+                     Building Commitment-Based
+                  </h3>
+                  <p>
+                     Since 2020, Our team at TOPP Tactical Intelligence Ltd of
+                     London has been dedicated in supporting Ohio-based
+                     behavioural health agency Alvis 180 in bringing a
+                     continuous improvement culture to its organization via an
+                     enhanced business process management program.
+                  </p>
+                  <h2
+                     id="cross-departmental-collaborations"
+                     class="wp-block-heading"
+                  >
+                     Cross-Departmental Collaborations
+                  </h2>
+                  <p>
+                     Since 2020, Our team at TOPP Tactical Intelligence Ltd of
+                     London has been dedicated in supporting Ohio-based
+                     behavioural health agency Alvis 180 in bringing a
+                     continuous improvement culture to its organization via an
+                     enhanced business process management program.
+                  </p>
+                  <h3
+                     id="a-case-study-in-kpi-governance"
+                     class="wp-block-heading"
+                  >
+                     A Case Study In KPI Governance
+                  </h3>
+                  <p>
+                     Since 2020, Our team at TOPP Tactical Intelligence Ltd of
+                     London has been dedicated in supporting Ohio-based
+                     behavioural health agency Alvis 180 in bringing a
+                     continuous improvement culture to its organization via an
+                     enhanced business process management program.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="pesto-products padding">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6 offset-lg-3">
+            <div class="section-title text-center">
+               <h2 class="title blue-gradient-ltr clip">Results to date</h2>
+            </div>
+         </div>
+      </div>
+      <div class="products">
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/svg_419283-1.png"
+                  alt="42 teams mapped"
+               />
+            </div>
+            <div class="product__content">
+               <h4>42 teams mapped</h4>
+               <p>
+                  among the in-scope business which included the core 3 key
+                  divisions
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/svg_488018-1.png"
+                  alt="50 processes"
+               />
+            </div>
+            <div class="product__content">
+               <h4>50 processes</h4>
+               <p>
+                  from the Behavioural Health business line focusing on
+                  productivity &amp; quality
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/svg_491779-1.png"
+                  alt="21 KPIs created"
+               />
+            </div>
+            <div class="product__content">
+               <h4>21 KPIs created</h4>
+               <p>
+                  capturing daily commitment activity and reporting monthly to
+                  10 middle &amp; senior level managers focusing on improvement
+                  opportunities
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/Vector.png"
+                  alt="25 monthly hours saved!"
+               />
+            </div>
+            <div class="product__content">
+               <h4>25 monthly hours saved!</h4>
+               <p>
+                  An estimated 25+ hours per month of administrative activities
+                  were eliminated from PRESTO's easy-to-use capture &amp;
+                  reporting automation
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/svg_309836-1.png"
+                  alt="4 PRESTO KPI Manager users trained"
+               />
+            </div>
+            <div class="product__content">
+               <h4>4 PRESTO KPI Manager users trained</h4>
+               <p>
+                  Using the quality team as a central point, 4 primary platform
+                  users became the KPI tracking and reporting experts
+                  responsible to meet monthly with the Alvis CEO and COO
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/svg_383960-1.png"
+                  alt="2.5 hours dedicated"
+               />
+            </div>
+            <div class="product__content">
+               <h4>2.5 hours dedicated</h4>
+               <p>
+                  Initial training on the PRESTO KPI Manager platform included a
+                  weekly 30-minute session with the 4 key users. In total, 2.5
+                  hours was all that was needed to see the team completely
+                  independent
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="video-section padding-bottom">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="section-title text-center">
+               <h2 class="title blue-gradient-ltr clip">
+                  Turning Lives Around
+               </h2>
+            </div>
+         </div>
+      </div>
+      <div
+         class="video-inner__wrap"
+         style="
+            background-image: url(https://www.prestopdca.com/wp-content/uploads/2025/07/Rectangle-146.png);
+         "
+      >
+         <div class="row align-items-center h-100">
+            <div class="col-md-12">
+               <div class="play__button text-center">
+                  <a
+                     href="https://www.youtube.com/watch?v=K4TOrB7at0Y"
+                     data-lightbox="video-1"
+                  >
+                     <img
+                        src="/wp-content/uploads/2025/07/play-button.webp"
+                        alt="Play Button"
+                     />
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative">
+   <div class="container">
+      <div class="row row-gap-5 align-items-center">
+         <div class="col-md-7 mb-5 mb-md-0">
+            <div class="started__content">
+               <h2>Get Started in Seconds</h2>
+               <div class="btn-wrap justify-content-start">
+                  <a
+                     href="https://www.prestopdca.com/contact-us/"
+                     class="btn btn-fill btn-icon icon-white"
+                     >Book a Demo</a
+                  >
+                  <a
+                     href="https://www.prestopdca.com/pricing/"
+                     class="btn btn-icon icon-black"
+                     >Get Started</a
+                  >
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="started__img">
+               <div class="media">
+                  <img
+                     src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp"
+                     alt="Get Started Image"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
