a711c1f6810a7f3894380b26734cc6db9fa12e54
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 16:24:37 +0530

    Add files via upload

---
+++ Learn/Case Studies/single-case/unops-2021-award-winning-clean-tech-start-up-chooses-presto.php
@@ -0,0 +1,211 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section class="case-study-detail padding">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8 col-md-6 mb-4 mb-md-0">
+            <h1 class="title">
+               UNOPS 2021 award-winning clean tech start-up chooses PRESTO
+            </h1>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="company__info element-space-30">
+               <div class="company__logo">
+                  <div class="media">
+                     <img
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/avsalt-logo.png"
+                        alt="Company Logo"
+                     />
+                  </div>
+               </div>
+
+               <div class="feature__list element-space-20">
+                  <div class="list__item">
+                     <h3>Industry</h3>
+                     <p>Green Energy</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Company size</h3>
+                     <p>Start-Up</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Teams</h3>
+                     <p>Company-wide</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Products used</h3>
+                     <p>Products used</p>
+                  </div>
+               </div>
+
+               <div class="btn-wrap justify-content-start">
+                  <a href="#" class="btn btn-fill" target=""> Enterprise </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="case-study-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 mb-4 mb-lg-0">
+				<div class="left-sidebar">
+					<div class="sidebar-list">
+						<h4 class="sub-title">In this story</h4>
+						<hr class="divider">
+						<ul><li><a class="side-link" href="#avsalt-chooses-presto-for-product-launch-strategy-deployment-long-term-operational-excellence-discipline">Avsalt chooses PRESTO for product launch, strategy deployment &amp; long-term operational excellence discipline</a></li><li><a class="side-link" href="#summary">Summary</a></li></ul>
+						<div class="btn-wrap justify-content-center mt-5">
+							<a href="#" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="case-study-description">
+					<div class="case-author_bio">
+						<div class="author__top  flex-wrap align-items-center">
+							<div class="author__img mb-3 mb-lg-0">
+																	<img src="https://www.prestopdca.com/wp-content/uploads/2025/07/sven-erik-01.jpg" alt="Svein Erik Sogn">
+															</div>
+							<div class="author__text ps-0">
+								<div class="author__name">
+									Svein Erik Sogn								</div>
+								<div class="author__designation">
+									CEO at Avsalt								</div>
+							</div>
+						</div>
+						<div class="author__desc">
+							" PRESTO is key in helping a CEO project vision into structured reality so that it can then be presented, enhanced and updated in real time together with all stakeholders "						</div>
+					</div>
+					<div class="post-full__content case-summary element-space-30">
+						<h2 id="avsalt-chooses-presto-for-product-launch-strategy-deployment-long-term-operational-excellence-discipline">Avsalt chooses PRESTO for product launch, strategy deployment &amp; long-term operational excellence discipline</h2>
+<h4 id="summary" class="wp-block-heading">Summary</h4>
+<p>Since October of 2020 we have been supporting Avsalt, Sweden’s newest clean water &amp; sanitation Startup who was recently awarded by the&nbsp;<a href="https://www.unops.org/" target="_blank" rel="noreferrer noopener">United Nations Office for Projects &amp; Services (UNOPS)</a>&nbsp;a spot in their 2021 boot camp program together with 4 other start-ups.</p>
+<p>Being a clean tech Startup, Avsalt has successfully completed the prototype phase, received 2 rounds of seed funding and is now entering the ‘scale up’ phase of its life cycle, a phase where unexpected challenges can often step in the way of the endless number of deliverables and projects already committed to its stakeholders.</p>
+<p>Avsalt CEO Svein Erik Sogn was looking for a solution to support strategy deployment, performance governance and daily discipline in handling all planned and unplanned tasks.</p>
+<p>Since the commencement last autumn, we have been partnering with avsalt’s C-suite as a startup mentor assisting in the framing of roles and responsibilities of the present team, cataloguing Avsalt processes, assigning accountability, and mapping outstanding activities to the mid-term and long term growth strategy.</p>
+<p>Leveraging our business management system PRESTO Digital Enterprise, the Avsalt leadership team has been able to articulate their long-term strategy in investor and client presentations while developing a daily discipline among the team to manage tasks and make tactical decisions based on urgency.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="pesto-products padding">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6 offset-lg-3">
+            <div class="section-title text-center">
+               <h2 class="title blue-gradient-ltr clip">Results to date</h2>
+            </div>
+         </div>
+      </div>
+      <div class="products">
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/minutes.png"
+                  alt="60 minutes"
+               />
+            </div>
+            <div class="product__content">
+               <h4>60 minutes</h4>
+               <p>
+                  was all that was needed to setup the company functional
+                  structure, personnel, and begin cataloguing target role &amp;
+                  responsibilities
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/Functional.svg"
+                  alt="12 Functional areas"
+               />
+            </div>
+            <div class="product__content">
+               <h4>12 Functional areas</h4>
+               <p>their personnel and roles &amp; responsibilities captured</p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/Processes.png"
+                  alt="65 Processes"
+               />
+            </div>
+            <div class="product__content">
+               <h4>65 Processes</h4>
+               <p>catalogued with key touch points identified</p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/projects.png"
+                  alt="40 projects"
+               />
+            </div>
+            <div class="product__content">
+               <h4>40 projects</h4>
+               <p>
+                  &amp; 397 supporting tasks planned, assigned and commenced
+                  with team members
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/strategic.svg"
+                  alt="10 strategic initiatives"
+               />
+            </div>
+            <div class="product__content">
+               <h4>10 strategic initiatives</h4>
+               <p>
+                  agreed, catalogued and linked to cascaded project portfolio
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative">
+   <div class="container">
+      <div class="row row-gap-5 align-items-center">
+         <div class="col-md-7 mb-5 mb-md-0">
+            <div class="started__content">
+               <h2>Get Started in Seconds</h2>
+               <div class="btn-wrap justify-content-start">
+                  <a href="#" class="btn btn-fill btn-icon icon-white"
+                     >Book a Demo</a
+                  >
+                  <a href="#" class="btn btn-icon icon-black">Get Started</a>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="started__img">
+               <div class="media">
+                  <img
+                     src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp"
+                     alt="Get Started Image"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
