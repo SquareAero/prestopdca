1b4404319e713dabc96e3edefe8249ee7da0376d
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 17:22:50 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/scaling-your-small-business.php
@@ -0,0 +1,136 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section
+   class="resources-detail padding"
+   style="
+      background-image: url(&quot;/wp-content/uploads/2025/07/Group-177.png&quot;);
+      background-size: cover;
+      background-repeat: no-repeat;
+   "
+>
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8">
+            <div class="resource__wrap element-space-20">
+               <h1 class="text-blue">Scaling your small business</h1>
+
+               <style>
+                  .text-blue {
+                     font-size: 24px;
+                  }
+               </style>
+
+               <h2 class="title">
+                  Put Your Small Business On Autopilot With Presto Business
+                  Management Software
+               </h2>
+
+               <div class="content">
+                  Wondering what is keeping your small business from reaching
+                  its maximum potential? Do you have doubts on your ability to
+                  scale your business and launch it to success? Download this
+                  6-part survey to learn if PRESTO P-D-C-A is a good fit for you
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4">
+            <div class="media">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/small-business-scalability-guide-1.png"
+                  alt="small business scaling system"
+               />
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<div class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses">
+    <div class="row">
+        <div class="col-sm-12">
+                        <h4 class="title text-center">Download Your Free Resource</h4>
+            
+            
+<div class="wpcf7 js" id="wpcf7-f2786-o1" lang="en-US" dir="ltr" data-wpcf7-id="2786">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+<form action="/resources/scaling-your-small-business/#wpcf7-f2786-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init" data-hs-cf-bound="true">
+<fieldset class="hidden-fields-container"><input type="hidden" name="_wpcf7" value="2786"><input type="hidden" name="_wpcf7_version" value="6.1"><input type="hidden" name="_wpcf7_locale" value="en_US"><input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f2786-o1"><input type="hidden" name="_wpcf7_container_post" value="0"><input type="hidden" name="_wpcf7_posted_data_hash" value=""><input type="hidden" name="_wpcf7_recaptcha_response" value="0cAFcWeA4QAqPVllsQfb5szqqrfI-obkhwIDN4MWWTiNm7t6_G3Nogwfx5OK7ZIxpjezov1ZDe_-navxIA0pA2S2p6n1enhbX9K9fT3R3wm_jjFQWx8WICwokw5-LTfD02xIppyteAWM3ErDLgGaeOyRJdpKYDmcyyh6zZ8doW6zQd_N6A-axRoBzq-rPJExvErb1IE2afGhJT4dpEGfko-E1LMXT8M-wkuxWiv7zKr3UGCb382RJP0owhhwfMIdlXJaE2Np5wuYHR-Y1HeInFUxK5LRnt27MeqIueBTbC2D8aaylK7N41pfr8QZOAlPO2KGRdKTlIAp2XP-1un1z3xqaXgx7wbDUwxJY200UFXhwnkDUIbqX5hW-eboenyViUPx-p4FXky-_1ahyixHU47UA963FmSB5Wx_Q3xb2n0AFDmN8EJJJuTo4VFT0RzpxuFtT1s5TI5watfpZJ05u-PJp7NFHtcjmo0Cprho8QVJyLhTILKS3HooBoBMsLbMgnziUb2-JC8r92EpuIzqa5gJkPLlGPDtHKJlFeBV7CZrfEk7s-k7WYgGcq_JFgCKEy1aqWO4Rwmq4NhFRea4oRQuvGWFeXKXWLCiY7ULPfBw12YpuWXOJj40e69TB7wRiR6DC_ap8BHCU0PA0_sQxTTESqlxUcX4aV2sjGJr14JQfcUvpKZvtRqdFU0urs7dNzfMU4qf4YTykzXhVDawAYVwYpih8p__gqGDPkdgSQhem454VlYO99jcLdrvS6s73QrCinxIAn7XRAe58rg7BZTEGmy8qThGgQTe4joUlyXyzBIX5cZPbnw3Itnh68_aJJXkxfTyS0yi6DezYh9f05ke3hdP4sOwRsl6fYf2qq3qrj0_HfV6_-bg7dRrBLqdbWVATyf0_eJ9KpEVxwIvlvrGhM6ZBR3P4wOLMuoyBsB8bTacY0PvLGigtCK4kUg7RAgwHwMgEkXtMmALf2xwr3MPbjOZeRtGJmWiI-hND61yeDrMu9PTUwE6NGGFBv3qR_NmrwmsirMjFIOBK9yVWz1c-65z3v2d5DqggubrEuqpKioZ1ZENGBwrE6a3yDlNTkS-ir2i-LWRkxKpaLG92Mptawm3z05OJFpH44KNonOFD-qfuPqqcA1x3iLnt7TGMFriAJFMUUXALWxXLF3P6blSva_mfATNXxivZdDtX-PO1g_QcAbtcd8pGGd2q5mQjKvgtmwqwbKbuwQi27rKh8lVkBgixWx64RwqOUaHBmE9PRmrVuOszfGX3lKftzeGyjkcnAoILuohij92LG93pTBY1zp2WTPaY35Hd3vtqNLNnhYY0Xyv58qptD42CLg_R7ts2-neI29nbM9G_535USWumlldYgGLAMit9KbOR_5QSOtXpRcPjqRdG8fsZP5bxImFFnch9YWfLa9MDXs4_PK5dB06Q-P7TX3DyQ9uDenzQSn5Z6V0bP3jDogZj_2pqtqEoeQdb0NOyBFsGlJV5YGyBCgN1ItlrAL-m9sv211DMFtqvDBcKIdP-a1xyyQj9kra9aY6NH1LL9YyrsAaIMhIZ8-y59fPo4Nq0zp7gR4CvlgtxLuslhif1nDD_R-eRDCZ_gy0fYKy-R2uu0LteE0fYn9oLinTIJhmDk1G0OumJDRzHyCsvkbFyQvSB49J0yVAyqT-ZnRhJABacl22V6n4FBsQhtIdyX5dUwA_BblC8nDzVQLz-jwLRynHttgudrlFdzhQFwu3gwR33wXeVzhZGdYo1A36VJW2DVALF0Vsw9cUJENjgYfwO8NPsHQZfFMiEGuknTcOvePxQVPab7fRlOofRBQMCQDNKaH0-hqQovBGMinf97intWBQXJEeOgAF7VE0SV1yKQ4Rqoa42YIUDddXqwPGrxFBKi9yq-hFe2MyoNNO7wPEaFtZH9omzBaOk7Y0tLrviqnnCP2UHdXpKWiHlMsumNcwKkN0Fi0LG_59i1XAmn7m5SfJhJWYgR5X_ryKWRbMDHWIDuC7Qjl9AszUcRPJB8ZmZ6A9a8cPgM1hRbXAlykeCQKa0slLE8KG-_Dvl9aqSHxw3m6b8P7YJ509HI1BRXFgOaurVE-0r6Ktkt_h7pblVkVP9S0ke9oeXB6orev_6eKjdhnbRa-uK5tIFpGU1b9DU5Bzd2S20Sxbf_L8Vu1B_MHbMSzL9yLu25D0glIuIP_6KDC7XzAa0s4bRtfQsGX3_NBBxxjBV3dhfFsTLmlxuiJtyC6zqLyy6qBWrD78rNtGdRLVdHUgwvcq_GtGH94w">
+</fieldset>
+<p><label> Your name<br>
+<span class="wpcf7-form-control-wrap" data-name="your-name"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" autocomplete="name" aria-required="true" aria-invalid="false" value="" type="text" name="your-name"></span> </label>
+</p>
+<p><label> Your email<br>
+<span class="wpcf7-form-control-wrap" data-name="your-email"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email" autocomplete="email" aria-required="true" aria-invalid="false" value="" type="email" name="your-email"></span> </label>
+</p>
+<p><input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit"><span class="wpcf7-spinner"></span>
+</p><div class="wpcf7-response-output" aria-hidden="true"></div>
+</form>
+</div>
+
+
+        </div>
+    </div>
+</div>
+
+<section class="field-guide ">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                                            <h2 class="title">
+                            We Have Been In The Market For More Than 7 Years Helping Small Businesses Scale Your Small Business By Working Smart And Offering More                            </h2>
+                    
+                    
+                    <div class="resource-bottom-desc element-space-20">
+                        
+                                                    <h2 class="icon-list-title">In this 6-part survey you will learn:</h2>
+                        
+                                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                        <ul class="custom-check-list">
+                                                                                            <li>What is holding you back from putting daily work on autopilot</li>
+                                                                                            <li>How to use Lean methodologies to bring structure and a sustainable growth discipline into your team</li>
+                                                                                            <li>How your business will become scalable by adopting the PRESTO P-D-C-A operating model methodology</li>
+                                                                                    </ul>
+                                    </div>
+                                                                    <div class="col-md-6">
+                                        <ul class="custom-check-list">
+                                                                                            <li>What our clients &amp; their clients have to say about using PRESTO P-D-C-A to scale their business</li>
+                                                                                            <li>And much more!</li>
+                                                                                    </ul>
+                                    </div>
+                                                            </div>
+                                            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
