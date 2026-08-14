03f11ba69cff1e68964459c2f88646e22c1901d4
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 17:25:24 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/document-file-media-mgmt.php
@@ -0,0 +1,146 @@
+
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+<section
+   class="resources-detail padding"
+   style="
+      background-image: url(https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png);
+      background-size: cover;
+      background-repeat: no-repeat;
+   "
+>
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8">
+            <div class="resource__wrap element-space-20">
+               <h1 class="text-blue">
+                  Document, file &amp; media mgmt&ZeroWidthSpace;
+               </h1>
+
+               <style>
+                  .text-blue {
+                     font-size: 24px;
+                  }
+               </style>
+
+               <h2 class="title">
+                  'When a man's knowledge is not in order, the more of it he
+                  has, the greater will be his confusion' - Herbert Spencer
+               </h2>
+
+               <div class="content">
+                  As your business grows, so is your online archive of support
+                  files &amp; documents making it more-and-more difficult to
+                  stay organised. Not having your documents easily available to
+                  those who need them, when they need them is costing you time,
+                  energy and money!
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4">
+            <div class="media">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/10_ways_to_revolutionise_your_biz_with_PRESTO_PP.webp"
+                  alt="document &amp; file management"
+               />
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<div class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses">
+    <div class="row">
+        <div class="col-sm-12">
+                        <h4 class="title text-center">Download Your Free Resource</h4>
+            
+            
+<div class="wpcf7 js" id="wpcf7-f2787-o1" lang="en-US" dir="ltr" data-wpcf7-id="2787">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+<form action="/resources/document-file-media-mgmt/#wpcf7-f2787-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init" data-hs-cf-bound="true">
+<fieldset class="hidden-fields-container"><input type="hidden" name="_wpcf7" value="2787"><input type="hidden" name="_wpcf7_version" value="6.1"><input type="hidden" name="_wpcf7_locale" value="en_US"><input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f2787-o1"><input type="hidden" name="_wpcf7_container_post" value="0"><input type="hidden" name="_wpcf7_posted_data_hash" value=""><input type="hidden" name="_wpcf7_recaptcha_response" value="0cAFcWeA7B_GScae7P3koBRxfZRxDN3K_lPQd-031VweQ3Hsd8ZDogQgqSbdPaXgTFd9gpJg-hyuCB3fNYWCGNkkkwiXHVOkqNwQpq2k6gmbb5j2ec3gEMiRAC_MH_dK5QplPwtdlNqmaP8EETC1Vdc18PlLQKch3unk4Qc-I28hFTXiwSdhC5r20Weo4EpYs3IuGz_EEdDj9Iyo3QvumazCN1_GTvJEWXZAcNg7OCPSnYnxdM6kv5FynLVe0N6CAqchg2eJ-PoeBdm8uZ69J7_QQk83aBQ8Ifj4BCceZup6qiBdodMBFZ9Ty8t4zCXrRjRzb4jZuOwjP9PocZvCQmbadiU_OLRnjhQkJAdpHz8Ad66zTD1la_fESecvZafGnAb3_0elVYrpjt-ZBGsnlGoZnVKarzNqIJ7P92reNi9WE8oyQl40sjuOi8CO9eRV1Oq6ffVDXehZwxdR8cLahvnNySkkIzI4BldAp9sD-eBX9djOkmel3Wx4GYUN0idR5ydd-w3BA7gsFlfYPwh1GITOcwMlLXhjWJ8F2HnQMZTlUxnIczOVRKdpM6NLNyxLQwiIvOSZq5HtxheNf4KLeBS0XUMK_EwHbIdjDkZgn-h8OvL4KEHaJDsDvBlIxfKD15XCma1McepVVmIl3xTgj9GNE9QRI4XPpNtfIVQM_u9MCg2RZTzs-PLVlNBj8CxUa7l-5ZjXKOr2WgZjKudNi9WYoD2CvHz-Lw40taIBSj2x5F2rElLlwwYX7VOtd0hxfqDOSYFdue42P_c09iHNxHo1R4XCfvCZ-k-Hsl1r2O2n4MOCmBaFsvZoITsv9LkqTB_FXbmnZwwRIy8nLDVXAFV0gO2AC4nvET3er-g4uei4QOiBGsgccacd3WQ8e2OJrLd2iPXy8Eqbg1LRpKXhHuCyZcEluaTNiDQbjWSiqeE98T6FNibqkbGp_tUGcvpjUThgyHSd3pFQiDRK2aMZFXuaVxati7Nv_W34ADc82H96W9c3Xbx3pJiPoxzimqvUrNh0WWpWl7A3ejj8DECVdIbv9F_wLt2XgWs-XOid9lrRv3QqN21pIkQ8eLMuw31fHydI5iE_3syuD2nyz2maXqTxzkhV1DCjgeDuqp7Hr4DC4Lau_zkII_rBAnNlRUc0JHRMbjKN90iSTAuqhWv2Bv_gpnphSFvSN3-Fbmo7i4cFBHcMxr-WkmyltMBMpTS-UN2y6Z90qyHf8wMHk41tGTVWBLDN9NIlpHrlZFqzVRToheOUK8TRI8mVMjAr36G9o0x8oxJ2GIz1JsoaFsIp9qWQ_bZd-zBWbL4PlOmxz610XKlS3LTyVfKP7na1BbWAbMsYcteDoibgBZkjJ2ffkbqaueDSJxFURQs2Am6uMmsLc0sEViTuAtoRUq4qsn9KQv-EgfE9g1c5Q5Vhr05OM9OJhIADQYnw9IQnexfHrsFBPgXanF1fdr_4D0kjU09fXA5Nr-l0R6BsfIJE3EtrllMrp-yrEKaSV6AeiK44qZ9qGk_KIfPofietRZ2I8AL3ORPYXqF4k4JlLs-YBs0D2nMG1howfBn8PzMcCF4c44847CymFPimKdQB4sLzTS9U_Ri4P4mkcTf5to0SqRxN1HQHgfDg1jcMe0uUrYJQI2vmf-kqRT_JCbC6UNEq8WLnCYi5cFldcjR_-BWeIRaaNTQpkY_U5NiXid2KiJt2CGy4mc9wm32cydlLcBpec0NuhPC9oNE3GStlfaHMht8HvSfuZqiDM2khz9FuJf-GvjlQXbz5XL5tgN72RsZC4i09q3nG7hC1AdJIAse2E1rYPV-8gmdZbZWi1ZDQmRbC3EP5ScJxQLRQlzrfmj6J3cT3me9HRMjojf2oYLNWgG8jSK8nL5RcHoOtefJLMBrBB39clKp7pAHvRS9y8YX5auqT2Yd4DzB-Tbd2M6yM6EJwbP8XVzRbgX5UItmVtuRFTt44vLIcwYBbSJRW31cxnrBUh9BW3bAUHnllDzMN3BXyeAjNW-2MWGky4_OdY2sE0JkrnGsyFv1Yr9kzMaiscGryDiaqVGVZMFlhUZl1cGK5-Q6MzDh1RisuFFV1sTGlWbS6vgqUAaRbmnwXNSEgpXXO7ouDJVLmMq7KOVwVwgWr2Lih3OBAcDVAc6Jc74Iy5fDhhyqItUXNk-70kdoQJtj2OhKNAzRqE7gzfrkyPY50GGSaS1IZqe3uvuLFA-5suAedY9NvKz-xyBSRiptgFkpKwRly8MUjRY5N4xxlA8TMC7MW8qxujlsITX2p2pLw-nNcvi3nmeIgcFoX341SEwhXyfcxBpNyMfV61e">
+</fieldset>
+<p><label> Your name<br>
+<span class="wpcf7-form-control-wrap" data-name="your-name"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" autocomplete="name" aria-required="true" aria-invalid="false" value="" type="text" name="your-name"></span> </label>
+</p>
+<p><label> Your email<br>
+<span class="wpcf7-form-control-wrap" data-name="your-email"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email" autocomplete="email" aria-required="true" aria-invalid="false" value="" type="email" name="your-email"></span> </label>
+</p>
+<p><input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit"><span class="wpcf7-spinner"></span>
+</p><div class="wpcf7-response-output" aria-hidden="true"></div>
+</form>
+</div>
+
+
+        </div>
+    </div>
+</div>
+
+
+<section class="field-guide ">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                                            <h2 class="title">
+                            Don't get left in the Digital Stone Age                            </h2>
+                    
+                    
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>Not having your documents properly aligned to your business value chain is costing you time, energy and money! Use PRESTO to become a paperless business and make document management fun while boosting productivity</p>
+<p><strong>SAVE TIME AND TAKE BACK CONTROL OF YOUR DOCUMENT MANAGEMENT PROGRAM WITH PRESTO P-D-C-A</strong></p>
+<p>Say goodbye to the stress related to document management. Standardise the way you manage media files across your company so everyone speaks the same language and become a true paperless office!</p>
+<p>Download this 10-step field guide to assess how paperless your company currently is and how PRESTO P-D-C-A can help you achieve digital excellence.</p>
+
+                                                    <h2 class="icon-list-title">In this guide you will learn:</h2>
+                        
+                                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                        <ul class="custom-check-list">
+                                                                                            <li>Why it is important to have your documents properly aligned to your business value chain</li>
+                                                                                            <li>How to create a streamlined and effective document maintenance workflow that you can copy &amp; paste throughout your company</li>
+                                                                                            <li>How to integrate the R-A-C-I matrix into your document management program to increase sense-of-urgency among document owners</li>
+                                                                                    </ul>
+                                    </div>
+                                                                    <div class="col-md-6">
+                                        <ul class="custom-check-list">
+                                                                                            <li>How PRESTO P-D-C-A's unique link-from-anywhere user interface decreases document retrieval time and increases productivity</li>
+                                                                                            <li>Where PRESTO P-D-C-A will improve the relationship with your clients</li>
+                                                                                            <li>The easiness PRESTO P-D-C-A will convince your team to stop printing documents and the immense cost savings resulting from it</li>
+                                                                                    </ul>
+                                    </div>
+                                                            </div>
+                                            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
