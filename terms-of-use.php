54f2c0fd9967d1e8f78fbd1339bef71c2809748d
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 15:02:14 +0530

    Add files via upload

---
+++ terms-of-use.php
@@ -0,0 +1,55 @@
+<?php include("header.php"); ?>
+<div id="" class="privacy-policy-banner ">
+
+    <div class="banner-privacy" style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/solution-bg.webp');">
+              <div class="container">
+           <div class="row">
+            <div class="col-md-12">
+            <div class="text-center section-title text-white">
+             <h1>Website Terms Of Use</h1>
+         </div>
+         </div>
+     </div>
+     </div>
+           
+ </div>
+
+<section class="privacy_content">
+   
+
+      <div class="container">
+           <div class="row">
+            <div class="col-md-12">
+                 <div class="content">
+            <h4>1. Website Terms of Use</h4>
+<p>This page sets out the Terms of Use for the www.TOPPTI.com website. Before using this website you should read our Privacy and Cookie Policies. By accessing and using our Website you are deemed to accept these policies. Do not use this Website if you do not agree to all of the following terms and conditions.</p>
+<p>This Website may contain links to third party websites, and if you access them then their policies will apply for the duration of your access to them.</p>
+<p>Any data or information you supply to ww.TOPPTI.com must comply with English Law and not defame any third party. You are responsible for any information that you provide or post on our Website.</p>
+<h4>2.Definitions and Interpretation</h4>
+<p>“you”, ”your” and “yours” means the person(s), you, who are accessing this Website and any party that you are accessing on their behalf</p>
+<p>“we”, “us” and “our” means TOPP Tactical Intelligence Limited (“TOPPTI”)</p>
+<p>“Website” means this website that you are currently on and any website operated by or on behalf of us</p>
+<h4>3.Intellectual Property Rights</h4>
+<p>All the text, graphics and other contents of this Website are copyrighted works and/or trademark protected materials of TOPPTI or its partners. Any product and company names mentioned on this Website may be trademarks or trade names of TOPPTI, its partners or third parties. You may not copy, modify, reproduce, distribute, transfer, reverse engineer, create derivative works from or store the contents or any part thereof in any form without the prior written consent of TOPPTI except as may be provided in these terms and conditions. Any rights not expressly granted herein are reserved by TOPPTI. Individual pages or documents may be subject to additional terms and conditions indicated in such pages or documents.</p>
+<h4>4.Acceptable Use</h4>
+<p>TOPPTI consents to you browsing this Website on your computer and printing extracts or copies of these pages or storing content on your computer for your personal, non-commercial use only unless otherwise provided in these terms and conditions or clearly indicated in the material you wish to use. You may use publications, software demos, online models and other material available for the internal purposes of your company, but you may not, under any circumstances, resell such material or the content thereof, or otherwise use the material for commercial purposes unless agreed to explicitly in writing by us. You may use press releases and other documents classified as public in public communications provided that the source of such information is clearly stated whenever used.</p>
+<h4>5.No Warranties – Limitation of Liability</h4>
+<p>His website and its content are provided for your convenience, “As is” and without warranties of any kind either express or implied, including those of title, non-infringement, merchantability and fitness for a particular purpose, or arising from a course of dealing, usage, or trade practice. Toppti does not warrant or make any representations regarding the use or the results of the use of the content or other materials in this website in terms of their correctness, accuracy, reliability, or otherwise. Toppti does not warrant that the use of this website is uninterrupted or that the website is error-free. Toppti reserves the right to revise this website (including this notice and the privacy policy) or withdraw access to it at its discretion. Toppti shall not be liable for any direct, indirect, special or consequential damages (including but not limited to lost profits) arising out of the use or inability to use this website or their content, even if we have been advised of the possibility of such damages. If a certain jurisdiction does not allow limitation or exclusion of certain liability, the above limitation shall apply to the greatest extent permitted by law.</p>
+<h4>6.Material You Submit</h4>
+<p>If you submit any information or material intended for inclusion on this Website you agree that such information and material will be deemed to be non-confidential and non-proprietary. By submitting unsolicited materials to us, you grant us a royalty free, unrestricted, irrevocable, perpetual license to use, reproduce, display, perform, modify, transmit and distribute those materials and information, unless otherwise agreed. If you submit any information or material to us by email or via this Website, you agree that: (i) you own all the material you submit to us or you have unlimited rights to use such material; (ii) you do not submit any unlawful material; and (iii) you will make all reasonable efforts to scan the material you submit for viruses and other contaminating features. You also agree that we does not accept any responsibility for the information or material submitted by you and you agree to indemnify us against any legal actions third parties may take regarding the material submitted by you.</p>
+<h4>7.External Links</h4>
+<p>We may offer links to other web sites. The linked sites are not under the control of TOPPTI and we are not responsible for the contents of any linked site or links within a linked site. A link to a third party site does not in any way imply that TOPPTI endorses such site or the contents therein. If you visit another site, you should review the privacy policy and other policies applicable to such site.</p>
+<h4>8.Privileged Areas</h4>
+<p>This Website may include privileged areas. You undertake to provide us with accurate information when registering for the use of the privileged areas. You also undertake to ensure the secrecy of your user identification. If you suspect that the secrecy of your user identification may have been compromised, you must immediately contact us. You shall be responsible for any activity occurring using your user identification.</p>
+       </div>
+         </div>
+     </div>
+
+
+        
+    </div>
+</section>
+
+</div>
+
+<?php include("footer.php"); ?>
