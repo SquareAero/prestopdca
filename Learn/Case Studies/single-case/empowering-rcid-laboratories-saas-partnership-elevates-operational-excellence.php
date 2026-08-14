088e836e23ac31097ef96dc26d85bc224766e61b
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 16:22:24 +0530

    Add files via upload

---
+++ Learn/Case Studies/single-case/empowering-rcid-laboratories-saas-partnership-elevates-operational-excellence.php
@@ -0,0 +1,295 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section class="case-study-detail padding">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8 col-md-6 mb-4 mb-md-0">
+            <h1 class="title">
+               Empowering RCID Laboratories: SaaS Partnership Elevates
+               Operational Excellence
+            </h1>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="company__info element-space-30">
+               <div class="company__logo">
+                  <div class="media">
+                     <img
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/alvis.png"
+                        alt="Company Logo"
+                     />
+                  </div>
+               </div>
+
+               <div class="feature__list element-space-20">
+                  <div class="list__item">
+                     <h3>Industry</h3>
+                     <p>Pharmaceuticals</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Company size</h3>
+                     <p>Mid-market</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Teams</h3>
+                     <p>Company-wide</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Products used</h3>
+                     <p>Products used</p>
+                  </div>
+               </div>
+
+               <div class="btn-wrap justify-content-start">
+                  <a href="#" class="btn btn-fill" target=""> Enterprise </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="case-study-content">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="left-sidebar">
+               <div class="sidebar-list">
+                  <h4 class="sub-title">In this story</h4>
+                  <hr class="divider" />
+                  <ul>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#bringing-the-future-of-diagnostics"
+                           >Bringing The Future Of Diagnostics</a
+                        >
+                     </li>
+                     <li><a class="side-link" href="#summary">Summary</a></li>
+                     <li>
+                        <a class="side-link" href="#quick-wins-accrued-to-date"
+                           >Quick Wins Accrued To Date</a
+                        >
+                     </li>
+                     <li>
+                        <a class="side-link" href="#seeking-iso-9001-excellence"
+                           >Seeking ISO 9001 Excellence</a
+                        >
+                     </li>
+                  </ul>
+                  <div class="btn-wrap justify-content-center mt-5">
+                     <a href="#" class="btn btn-fill btn-icon icon-white"
+                        >Book a Demo</a
+                     >
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-8">
+            <div class="case-study-description">
+               <div class="case-author_bio">
+                  <div class="author__top flex-wrap align-items-center">
+                     <div class="author__img mb-3 mb-lg-0">
+                        <img
+                           src="https://www.prestopdca.com/wp-content/uploads/2025/07/shruticatamrakar-1.webp"
+                           alt="Dr. Shrutica Tamrakar"
+                        />
+                     </div>
+                     <div class="author__text ps-0">
+                        <div class="author__name">Dr. Shrutica Tamrakar</div>
+                        <div class="author__designation">
+                           Founder &amp; Director at RCID Laboratories
+                        </div>
+                     </div>
+                  </div>
+                  <div class="author__desc">
+                     "I always had the vision to serve the people of the city in
+                     which I grew up and as the fate stands, I started my own
+                     Specialised Infectious Diseases laboratories RCID
+                     Laboratories"
+                  </div>
+               </div>
+               <div class="post-full__content case-summary element-space-30">
+                  <h2 id="bringing-the-future-of-diagnostics">
+                     Bringing The Future Of Diagnostics
+                  </h2>
+                  <h3 id="summary" class="wp-block-heading">Summary</h3>
+                  <p>
+                     Equipped with all the latest equipment required for the
+                     diagnosis of infectious diseases and having more than 300
+                     types of tests available, we are delighted to be working
+                     with Indian start-up RCID Laboratories of Bhilai.
+                  </p>
+                  <p>
+                     Founded in 2023 by Dr. Shrutica Tamrakar, RCID Labs hired
+                     30+ staff, including 18 lab technicians and has strong
+                     administration support in Legal, HR, Sales, Marketing &amp;
+                     Procurement. Dr. Tamrakar’s vast experience from esteemed
+                     clinics like Calcutta Medical Research Institute and Shri
+                     Shankaracharya Institute of Medical Sciences forms the
+                     foundation of this venture.
+                  </p>
+                  <p>
+                     Our collaboration started this summer after being contacted
+                     by new-hire COO Amit Saurabh looking for a digital
+                     transformation partner to structure his KPI-focused
+                     performance management program and track change projects
+                     across the ever-changing organisational landscape of the
+                     new-found company.
+                  </p>
+                  <h2 id="quick-wins-accrued-to-date" class="wp-block-heading">
+                     Quick Wins Accrued To Date
+                  </h2>
+                  <ol class="wp-block-list">
+                     <li>
+                        <strong>Change project milestones mapped:</strong
+                        >&nbsp;Projects &amp; goals of each team leaders based
+                        on corporate strategic initiatives catalogued
+                     </li>
+                     <li>
+                        <strong>Assigned target metrics:</strong>Agree
+                        improvements areas in productivity, quality and employee
+                        engagement
+                     </li>
+                     <li>
+                        <strong>Create daily discipline:</strong>Activate PRESTO
+                        KPI management software training underway to track &amp;
+                        support daily performance of 6 team leaders
+                     </li>
+                     <li>
+                        <strong>Report to management:</strong>Initial reporting
+                        set and review cadence proposed to Senior Management to
+                        ensure proactive action-taking in situations of under
+                        performance
+                     </li>
+                  </ol>
+                  <h2 id="seeking-iso-9001-excellence" class="wp-block-heading">
+                     Seeking ISO 9001 Excellence
+                  </h2>
+                  <p>
+                     Regional clinical and infectious diseases Laboratory (RCID
+                     Lab) is an ISO 9001:2015 certified Govt approved
+                     specialised infectious disease testing laboratory. This
+                     marks our second ISO 9001 certified small-client initiative
+                     in the past 2 months indicating once again that our
+                     operational excellence platform and service offering is for
+                     the quality-focused start-up looking for structure and
+                     discipline in their operating model governance program.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="pesto-products padding">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6 offset-lg-3">
+            <div class="section-title text-center">
+               <h2 class="title blue-gradient-ltr clip">Results to date</h2>
+            </div>
+         </div>
+      </div>
+      <div class="products">
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/1-1.svg"
+                  alt="Teams mapped"
+               />
+            </div>
+            <div class="product__content">
+               <h4>Teams mapped</h4>
+               <p>
+                  Organisational landscape available on PRESTO P-D-C-A dashboard
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/Processes-catalogued.svg"
+                  alt="Processes catalogued"
+               />
+            </div>
+            <div class="product__content">
+               <h4>Processes catalogued</h4>
+               <p>Initial process portfolio drafted &amp; captured per team</p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/3-users-1.svg"
+                  alt="Initial KPIs agreed"
+               />
+            </div>
+            <div class="product__content">
+               <h4>Initial KPIs agreed</h4>
+               <p>
+                  Capturing daily commitment activity for team leaders and
+                  monthly reporting to senior level managers focusing on
+                  improvement opportunities
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/Initial-KPIs.svg"
+                  alt="3 users trained"
+               />
+            </div>
+            <div class="product__content">
+               <h4>3 users trained</h4>
+               <p>
+                  Using the Senior Team as a central point, 3 primary platform
+                  users are well on their way to being PRESTO P-D-C-A product
+                  champions
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative">
+   <div class="container">
+      <div class="row row-gap-5 align-items-center">
+         <div class="col-md-7 mb-5 mb-md-0">
+            <div class="started__content">
+               <h2>Get Started in Seconds</h2>
+               <div class="btn-wrap justify-content-start">
+                  <a
+                     href="https://www.prestopdca.com/contact-us/"
+                     class="btn btn-fill btn-icon icon-white"
+                     >Book a Demo</a
+                  >
+                  <a
+                     href="https://www.prestopdca.com/pricing/"
+                     class="btn btn-icon icon-black"
+                     >Get Started</a
+                  >
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="started__img">
+               <div class="media">
+                  <img
+                     src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp"
+                     alt="Get Started Image"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
