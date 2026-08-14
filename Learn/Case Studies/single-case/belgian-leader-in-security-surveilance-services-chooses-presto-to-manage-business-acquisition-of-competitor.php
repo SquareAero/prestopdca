7473a2782e47af0faea0f8d2961e94dfb51bb9c0
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 16:19:13 +0530

    Add files via upload

---
+++ Learn/Case Studies/single-case/belgian-leader-in-security-surveilance-services-chooses-presto-to-manage-business-acquisition-of-competitor.php
@@ -0,0 +1,378 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+<section class="case-study-detail padding">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8 col-md-6 mb-4 mb-md-0">
+            <h1 class="title">
+               Belgian leader in security surveilance services chooses PRESTO to
+               manage business acquisition of competitor
+            </h1>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="company__info element-space-30">
+               <div class="company__logo">
+                  <div class="media">
+                     <img
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/06/protectionunit-1.png"
+                        alt="Company Logo"
+                     />
+                  </div>
+               </div>
+
+               <div class="feature__list element-space-20">
+                  <div class="list__item">
+                     <h3>Industry</h3>
+                     <p>Security Professional Services</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Company size</h3>
+                     <p>Mid-market</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Teams</h3>
+                     <p>Company-wide</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Products used</h3>
+                     <p>KPI Reporting &amp; Hoshin Kanri Strategy</p>
+                  </div>
+               </div>
+
+               <div class="btn-wrap justify-content-start">
+                  <a href="#" class="btn btn-fill" target=""> Enterprise </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="case-study-content">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="left-sidebar">
+               <div class="sidebar-list">
+                  <h4 class="sub-title">In this story</h4>
+                  <hr class="divider" />
+                  <ul>
+                     <li>
+                        <a class="side-link" href="#the-partnership"
+                           >The partnership</a
+                        >
+                     </li>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#from-spreadsheets-to-real-time-cloud-based-performance-management"
+                           >From spreadsheets to real-time, cloud-based
+                           performance management</a
+                        >
+                     </li>
+                     <li>
+                        <a class="side-link" href="#objectives">OBJECTIVES</a>
+                     </li>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#case-study-details-download-here"
+                           >Case Study Details Download Here</a
+                        >
+                     </li>
+                  </ul>
+                  <div class="btn-wrap justify-content-center mt-5">
+                     <a href="#" class="btn btn-fill btn-icon icon-white"
+                        >Book a Demo</a
+                     >
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-8">
+            <div class="case-study-description">
+               <div class="case-author_bio">
+                  <div class="author__top flex-wrap align-items-center">
+                     <div class="author__img mb-3 mb-lg-0">
+                        <img
+                           src="https://www.prestopdca.com/wp-content/uploads/2026/01/nicolas-deangelis.jpg"
+                           alt="Nicolas De Angelis"
+                        />
+                     </div>
+                     <div class="author__text ps-0">
+                        <div class="author__name">Nicolas De Angelis</div>
+                        <div class="author__designation">CEO</div>
+                     </div>
+                  </div>
+                  <div class="author__desc">
+                     " PRESTO offers a quick and easy way to stay on top of all
+                     my cross-departmental relationships giving me the
+                     non-confrontational reporting package I need to propose
+                     improvement opportunities in service levels &amp;
+                     interactions "
+                  </div>
+               </div>
+               <div class="post-full__content case-summary element-space-30">
+                  <p>
+                     Protection Unit is a Belgian company established in 2008
+                     and specialising in the supply of security services. Its
+                     mission is to anticipate and to respond professionally to
+                     the needs of its customers while increasing the
+                     satisfaction of staff members by developing their skills,
+                     their versatility, their autonomy, their sense of
+                     responsibility and their motivation in compliance with
+                     quality standards.
+                  </p>
+                  <p>
+                     In the summer of 2019, Price Waterhouse Coopers (PwC)
+                     contacted us to assist their client, Protection Unit with
+                     the restructuring of the company as it was merging with its
+                     former competitor Fact Group and needed a robust digital
+                     solution to capture and catalogue business processes,
+                     report KPIs and cascade the long-term strategic vision of
+                     the organisation into measurable annual objectives.
+                  </p>
+                  <h2 id="the-partnership">The partnership</h2>
+                  <p>
+                     To support Protection Unit’s 3 major offices (Liege,
+                     Brussels &amp; Antwerp), we chose Luxembourg-based MK PM
+                     Consulting to take the lead role in the product
+                     installation to ensure quality training of the Senior
+                     Management Team and accuracy in the data upload consisting
+                     of 400 circa new KPIs.
+                  </p>
+                  <p>
+                     URL&nbsp;<a
+                        href="http://www.protectionunit.com/en/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        >www.protectionunit.com</a
+                     >
+                  </p>
+                  <h3
+                     id="from-spreadsheets-to-real-time-cloud-based-performance-management"
+                     class="wp-block-heading"
+                  >
+                     From spreadsheets to real-time, cloud-based performance
+                     management
+                  </h3>
+                  <p>
+                     With digital transformation on the top of its agenda and
+                     looking for a more effective way to measure and govern
+                     daily business activity, Protection Unit chose PRESTO KPI
+                     as its KPI management solution to accelerate tactical
+                     decision making and remove the risks and administrative
+                     burden of using off-line solutions (e.g. spreadsheets) to
+                     manage its 350+ KPI portfolio.
+                  </p>
+                  <h3 id="objectives" class="wp-block-heading">OBJECTIVES</h3>
+                  <ol class="wp-block-list">
+                     <li>
+                        Put structure to the post-acquisition organisation
+                        giving visibility into functions, roles &amp;
+                        responsibilities
+                     </li>
+                     <li>
+                        Group the portfolio of 350+ KPIs into categories and
+                        sub-categories to allow for quicker accessibility to
+                        information in times of necessity
+                     </li>
+                     <li>
+                        Support 16 Division Heads in the handoff and training of
+                        the KPI management solution PRESTO
+                     </li>
+                     <li>
+                        Capture the taxonomy of business processes at the team
+                        level allowing team leads to link underperforming KPIs
+                        to process optimisation opportunities
+                     </li>
+                     <li>
+                        Reduce time dedicated by stakeholders to excessive
+                        documentation, paperwork and communication
+                     </li>
+                  </ol>
+                  <p>
+                     <img
+                        fetchpriority="high"
+                        decoding="async"
+                        class="alignnone size-full wp-image-1416"
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/protectionunit03.jpg"
+                        alt="post acquisition structure"
+                        width="240"
+                        height="239"
+                        srcset="
+                           https://www.prestopdca.com/wp-content/uploads/2025/07/protectionunit03.jpg         240w,
+                           https://www.prestopdca.com/wp-content/uploads/2025/07/protectionunit03-150x150.jpg 150w
+                        "
+                        sizes="(max-width: 240px) 100vw, 240px"
+                     />
+                  </p>
+                  <h2
+                     id="case-study-details-download-here"
+                     class="wp-block-heading"
+                  >
+                     <a
+                        href="https://www.prestopdca.com/wp-content/uploads/2025/07/Protection_Unit_Case_Study.pdf"
+                        data-type="link"
+                        data-id="/wp-content/uploads/2025/07/Protection_Unit_Case_Study.pdf"
+                        >Case Study Details Download Here</a
+                     >
+                  </h2>
+                  <p>
+                     <a
+                        href="https://www.prestopdca.com/wp-content/uploads/2025/07/Protection_Unit_Case_Study.pdf"
+                        ><img
+                           decoding="async"
+                           class="alignnone wp-image-1417 size-full"
+                           src="https://www.prestopdca.com/wp-content/uploads/2025/07/publicity-05c.jpg"
+                           alt="business acquisition management"
+                           width="895"
+                           height="516"
+                           srcset="
+                              https://www.prestopdca.com/wp-content/uploads/2025/07/publicity-05c.jpg         895w,
+                              https://www.prestopdca.com/wp-content/uploads/2025/07/publicity-05c-300x173.jpg 300w,
+                              https://www.prestopdca.com/wp-content/uploads/2025/07/publicity-05c-768x443.jpg 768w
+                           "
+                           sizes="(max-width: 895px) 100vw, 895px"
+                     /></a>
+                  </p>
+                  <p>&nbsp;</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="pesto-products padding">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6 offset-lg-3">
+            <div class="section-title text-center">
+               <h2 class="title blue-gradient-ltr clip">Results to date</h2>
+            </div>
+         </div>
+      </div>
+      <div class="products">
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/minutes.png"
+                  alt="6 hours (cumulative)"
+               />
+            </div>
+            <div class="product__content">
+               <h4>6 hours (cumulative)</h4>
+               <p>
+                  was all that was needed to get the management team
+                  up-and-running through videos and online remote support
+                  sessions
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/Functional.svg"
+                  alt="16 team leaders"
+               />
+            </div>
+            <div class="product__content">
+               <h4>16 team leaders</h4>
+               <p>
+                  &amp; their personnel and roles and responsibilities captured
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/350-KPIs.svg"
+                  alt="350+ KPIs"
+               />
+            </div>
+            <div class="product__content">
+               <h4>350+ KPIs</h4>
+               <p>
+                  Loaded &amp; ready on day 1 of training for the team leaders
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/10-reports.svg"
+                  alt="10 reports"
+               />
+            </div>
+            <div class="product__content">
+               <h4>10 reports</h4>
+               <p>
+                  introduced, agreed and actively used to communicate
+                  operational variance and improvement opportunities
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/24-hours.svg"
+                  alt="24 hours per month"
+               />
+            </div>
+            <div class="product__content">
+               <h4>24 hours per month</h4>
+               <p>
+                  $aved on administrative activities (elimination of
+                  spreadsheets to manage data retrieval, report compilation,
+                  etc.)
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="get-started bg-light-gray position-relative">
+   <div class="container">
+      <div class="row row-gap-5 align-items-center">
+         <div class="col-md-7 mb-5 mb-md-0">
+            <div class="started__content">
+               <h2>Get Started in Seconds</h2>
+               <div class="btn-wrap justify-content-start">
+                  <a
+                     href="https://www.prestopdca.com/contact-us/"
+                     class="btn btn-fill btn-icon icon-white"
+                     >Book a Demo</a
+                  >
+                  <a
+                     href="https://www.prestopdca.com/pricing/"
+                     class="btn btn-icon icon-black"
+                     >Get Started</a
+                  >
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="started__img">
+               <div class="media">
+                  <img
+                     src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp"
+                     alt="Get Started Image"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
