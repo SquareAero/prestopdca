aaabf7d2504856b0843c627a831f99590ff3e6e8
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 16:06:14 +0530

    Add files via upload

---
+++ Learn/Case Studies/single-case/luxembourg-insurance-company-completes-phase-1-of-company-wide-process-orchestration-deployment.php
@@ -0,0 +1,335 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+<section class="case-study-detail padding">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-8 col-md-6 mb-4 mb-md-0">
+            <h1 class="title">
+               Luxembourg Insurance company completes phase 1 of company-wide
+               process orchestration deployment
+            </h1>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="company__info element-space-30">
+               <div class="company__logo">
+                  <div class="media">
+                     <img
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/logo-builders-2.png"
+                        alt="Company Logo"
+                     />
+                  </div>
+               </div>
+
+               <div class="feature__list element-space-20">
+                  <div class="list__item">
+                     <h3>Industry</h3>
+                     <p>Financial Services</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Company size</h3>
+                     <p>Mid-market</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Teams</h3>
+                     <p>Company-wide</p>
+                  </div>
+                  <div class="list__item">
+                     <h3>Products used</h3>
+                     <p>Products used</p>
+                  </div>
+               </div>
+
+               <div class="btn-wrap justify-content-start">
+                  <a href="#" class="btn btn-fill" target=""> Enterprise </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="case-study-content">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="left-sidebar">
+               <div class="sidebar-list">
+                  <h4 class="sub-title">In this story</h4>
+                  <hr class="divider" />
+                  <ul>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#reinventing-re-insurance-by-creating-new-synergies"
+                           >Reinventing (re)insurance by creating new
+                           synergies</a
+                        >
+                     </li>
+                     <li><a class="side-link" href="#summary">Summary</a></li>
+                     <li>
+                        <a class="side-link" href="#the-p-o-c-proof-of-concept"
+                           >The P.O.C. (proof-of-concept)</a
+                        >
+                     </li>
+                     <li>
+                        <a
+                           class="side-link"
+                           href="#case-study-complete-details-download-here"
+                           >Case Study Complete Details Download Here</a
+                        >
+                     </li>
+                  </ul>
+                  <div class="btn-wrap justify-content-center mt-5">
+                     <a href="#" class="btn btn-fill btn-icon icon-white"
+                        >Book a Demo</a
+                     >
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-8">
+            <div class="case-study-description">
+               <div class="case-author_bio">
+                  <div class="author__top flex-wrap align-items-center">
+                     <div class="author__img mb-3 mb-lg-0">
+                        <img
+                           src="https://www.prestopdca.com/wp-content/uploads/2026/01/roland-maitrejean.jpg"
+                           alt="Roland Maitrejean"
+                        />
+                     </div>
+                     <div class="author__text ps-0">
+                        <div class="author__name">Roland Maitrejean</div>
+                        <div class="author__designation">PMO Director</div>
+                     </div>
+                  </div>
+                  <div class="author__desc">
+                     " PRESTO offers a quick and easy way to stay on top of all
+                     my cross-departmental relationships giving me the
+                     non-confrontational reporting package I need to propose
+                     improvement opportunities in service levels &amp;
+                     interactions "
+                  </div>
+               </div>
+               <div class="post-full__content case-summary element-space-30">
+                  <h2 id="reinventing-re-insurance-by-creating-new-synergies">
+                     Reinventing (re)insurance by creating new synergies
+                  </h2>
+                  <p>
+                     Presto was recently implemented in one of the largest and
+                     most successful insurance and reinsurance businesses
+                     operating in Luxembourg today. Comprising a direct insurer
+                     and a reinsurer, the Insurance Group is ideally structured
+                     to offer complete insurance and reinsurance services for a
+                     wide range of risk exposures and business activities across
+                     the globe.
+                  </p>
+                  <h2 id="summary" class="wp-block-heading">Summary</h2>
+                  <p>
+                     Since July of 2021 we have been working with a
+                     Luxemnbourg-based insurance company in the assistance of
+                     putting a sustainable digital structure to the newly
+                     established Project Management and Process Orchestration
+                     Governance Program. By way of our enterprise business
+                     management software-as-a-service solution&nbsp;<strong
+                        >Presto PDCA</strong
+                     >, we are delighted to report ongoing achievements in
+                     multiple areas of operational excellence and digital
+                     transformation innovation that our teams have jointly
+                     brought to market since our successful product launch in
+                     2021.
+                  </p>
+                  <p>
+                     This summer we completed our first proof-of-concept
+                     exercise focusing on process orchestration and conducted
+                     within the Human Resources Department with the assistance
+                     of Pricewaterhouse Coopers of Luxembourg.
+                  </p>
+                  <p>
+                     <img
+                        decoding="async"
+                        class="alignnone size-full wp-image-1423"
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/topp-tactical-intelligence-02.png"
+                        alt="topp tactical intelligence logo"
+                        width="200"
+                        height="92"
+                     />
+                  </p>
+                  <p>
+                     <img
+                        fetchpriority="high"
+                        decoding="async"
+                        class="alignnone size-medium wp-image-1424"
+                        src="https://www.prestopdca.com/wp-content/uploads/2025/07/pwc-300x169.png"
+                        alt="PwC logo"
+                        width="300"
+                        height="169"
+                        srcset="
+                           https://www.prestopdca.com/wp-content/uploads/2025/07/pwc-300x169.png    300w,
+                           https://www.prestopdca.com/wp-content/uploads/2025/07/pwc-1024x576.png  1024w,
+                           https://www.prestopdca.com/wp-content/uploads/2025/07/pwc-768x432.png    768w,
+                           https://www.prestopdca.com/wp-content/uploads/2025/07/pwc-1536x864.png  1536w,
+                           https://www.prestopdca.com/wp-content/uploads/2025/07/pwc-2048x1152.png 2048w
+                        "
+                        sizes="(max-width: 300px) 100vw, 300px"
+                     />
+                  </p>
+                  <h2 id="the-p-o-c-proof-of-concept" class="wp-block-heading">
+                     The P.O.C. (proof-of-concept)
+                  </h2>
+                  <p>
+                     Being a process-focused organisation, flexibility and
+                     out-of-the-box thinking were core principles when agreeing
+                     the first process orchestration test pilot’s core
+                     objectives and approach.
+                  </p>
+                  <p>
+                     The proof-of-concept was conducted over a 4-week period
+                     within the Human Resources Department under the supervision
+                     of the Project Management Office (PMO). In addition to
+                     testing software usability and functionality, the team
+                     collectively identified a rich portfolio of product
+                     enhancement opportunities that resulted in the launch of an
+                     additional enterprise-wide continuous improvement product
+                     development initiative
+                  </p>
+                  <p>
+                     By way of the online training showrooms designed and tested
+                     by the core project team, two work sessions were all that
+                     was needed to train the Human Resources Department to be
+                     completely independent using PRESTO’s interface
+                  </p>
+                  <h4
+                     id="case-study-complete-details-download-here"
+                     class="wp-block-heading"
+                  >
+                     <a
+                        href="https://www.prestopdca.com/wp-content/uploads/2025/07/Builders-Generic-Case-Study.pdf"
+                        >Case Study Complete Details Download Here</a
+                     >
+                  </h4>
+                  <p>
+                     <a
+                        href="https://www.prestopdca.com/wp-content/uploads/2025/07/Builders-Generic-Case-Study.pdf"
+                        ><img
+                           decoding="async"
+                           class="alignnone wp-image-1427 size-full"
+                           src="https://www.prestopdca.com/wp-content/uploads/2025/07/builders-05.jpg"
+                           alt="proof concept testing"
+                           width="996"
+                           height="559"
+                           srcset="
+                              https://www.prestopdca.com/wp-content/uploads/2025/07/builders-05.jpg         996w,
+                              https://www.prestopdca.com/wp-content/uploads/2025/07/builders-05-300x168.jpg 300w,
+                              https://www.prestopdca.com/wp-content/uploads/2025/07/builders-05-768x431.jpg 768w
+                           "
+                           sizes="(max-width: 996px) 100vw, 996px"
+                     /></a>
+                  </p>
+                  <p>&nbsp;</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<section class="pesto-products padding">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6 offset-lg-3">
+            <div class="section-title text-center">
+               <h2 class="title blue-gradient-ltr clip">Results to date</h2>
+            </div>
+         </div>
+      </div>
+      <div class="products">
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/25-audit.svg"
+                  alt="1 monthly process orchestrated"
+               />
+            </div>
+            <div class="product__content">
+               <h4>1 monthly process orchestrated</h4>
+               <p>
+                  The monthly salary payment process owned by Human Resources
+                  consisting of 35 process steps was scheduled and governed via
+                  PRESTO's process workflow kanban
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/3-users.svg"
+                  alt="4 users trained"
+               />
+            </div>
+            <div class="product__content">
+               <h4>4 users trained</h4>
+               <p>
+                  4 users were all that was needed to ensure all core
+                  information was captured and reported correctly
+               </p>
+            </div>
+         </div>
+         <div class="product__box">
+            <div class="product__icon">
+               <img
+                  src="https://www.prestopdca.com/wp-content/uploads/2025/07/85-time.svg"
+                  alt="85% time savings!"
+               />
+            </div>
+            <div class="product__content">
+               <h4>85% time savings!</h4>
+               <p>
+                  Considering each process step and all the offline file
+                  management used to support each, PRESTO allowed for all of
+                  this to be completed in one easy-to-access place and in a much
+                  safer manner
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="get-started bg-light-gray position-relative">
+   <div class="container">
+      <div class="row row-gap-5 align-items-center">
+         <div class="col-md-7 mb-5 mb-md-0">
+            <div class="started__content">
+               <h2>Get Started in Seconds</h2>
+               <div class="btn-wrap justify-content-start">
+                  <a
+                     href="https://www.prestopdca.com/contact-us/"
+                     class="btn btn-fill btn-icon icon-white"
+                     >Book a Demo</a
+                  >
+                  <a
+                     href="https://www.prestopdca.com/pricing/"
+                     class="btn btn-icon icon-black"
+                     >Get Started</a
+                  >
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="started__img">
+               <div class="media">
+                  <img
+                     src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp"
+                     alt="Get Started Image"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
