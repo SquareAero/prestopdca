26862a3f6126c977928c21105c7be77c59c44f74
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-14 15:03:28 +0530

    Add files via upload

---
+++ privacy-policy.php
@@ -0,0 +1,103 @@
+<?php include("header.php"); ?>
+
+<div id="" class="privacy-policy-banner ">
+
+    <div class="banner-privacy" style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/solution-bg.webp');">
+              <div class="container">
+           <div class="row">
+            <div class="col-md-12">
+            <div class="text-center section-title text-white">
+             <h1>Privacy Policy</h1>
+         </div>
+         </div>
+     </div>
+     </div>
+           
+ </div>
+
+<section class="privacy_content">
+   
+
+      <div class="container">
+           <div class="row">
+            <div class="col-md-12">
+                 <div class="content">
+            <h4 class="style_6">Privacy Policy</h4>
+<p class="style_7">This privacy policy together with our Terms of Use, Cookie Policy and any other agreement between you and us sets out how TOPP Tactical Intelligence Limited of 27 Old Gloucester Street, London WC1N 3AX, company number 10250761 collects, stores and uses any information, including personal data, that you give to us when you visit our website or use our products and services.</p>
+<h4 class="style_6">Definitions and Interpretation</h4>
+<p class="style_7">“you”, ”your” and “yours” means the person(s), you, who are accessing this Website and any party that you are accessing on their behalf “we”, “us” and “our” means TOPP Tactical Intelligence Limited “Website” means this website that you are currently on and any website operated by or on behalf of us For the purposes of the General Data Protection Regulations the data controller is TOPP Tactical Intelligence Limited of 27 Old Gloucester Street, London WC1N 3AX</p>
+<p class="style_7">We are committed to ensuring that your privacy is protected at all times. Should we ask you to provide certain information by which you can be identified when visiting our Website or using our products and services, then you can be assured that it will only be used in accordance with this privacy policy. This privacy policy applies where we are acting as a data controller with respect to your personal data.</p>
+<p class="style_7">We reserve the right to amend this privacy policy from time to time in our sole discretion. Although we will use all reasonable efforts to notify you of substantial changes to this privacy policy, you should check this page on a regular basis to ensure that you are happy with any changes.</p>
+<p class="style_7">It is important that the personal data we hold about you is accurate and current. Please keep us informed if your personal data changes during your relationship with us. You can ask us to modify, update or delete your personal data at any time by contacting us at . Please note that there may be occasions where we have legitimate business or legal purposes to keep your personal data even if you ask us to delete it.</p>
+<p class="style_7">Our Website may contain links to third party websites, plug-ins or applications which may allow third parties to collect or share your personal data. However, once you have used these links to leave our Website, we do not have any control over that other website. Therefore, we cannot be responsible for the protection and privacy of any information which you provide whilst visiting such sites and such sites are not governed by this privacy policy. When you leave our website we encourage you to read the privacy policy of every website you visit.</p>
+<p class="style_7">You may request details of the personal data which we hold about you under the General Data Protection Regulation. If you would like a copy of the information held on you, please e-mail us at</p>
+<p class="style_7">We have appointed a data protection officer who is responsible for overseeing questions in relation to this privacy notice. If you have any questions relating to it, including any requests to exercise your legal rights, please contact us at .</p>
+<h4 class="style_6">1.Data Protection principles</h4>
+<p class="style_7">We are committed to complying with the principles of data protection as set out in the GDPR and the data protection law applicable to your jurisdiction. We will make every effort to comply with these principles. Personal data must:</p>
+<ul class="note-privcy">
+<li>Be processed lawfully, fairly and in a transparent manner</li>
+<li>Be obtained only for a specific, lawful purpose</li>
+<li>Be adequate, relevant and limited to what is necessary</li>
+<li>Be accurate and, where necessary, kept up to date</li>
+<li>Not be held for any longer than necessary</li>
+<li>Be protected and safeguarded in appropriate ways</li>
+</ul>
+<h4 class="style_6">2.The type of information we collect</h4>
+<p class="style_7">We may collect both personal and non-personal data. Personal data is defined by the General Data Protection Regulation (EU Regulation 2016/679) (“GDPR”) and means any information about an individual from which that person can be identified. Non-personal data means all other information collected that does not enable you to be identified.</p>
+<p class="style_7">We may collect, use, store and process different kinds of personal and non-personal data either through data you have chosen to make available when visiting our website, attending webinars, requesting demonstrations or other downloads, or through asking us to contact you. We will also collect other data when either you or your employer uses our products and services.</p>
+<p class="style_7">Personal data we collect may include your name, address, e-mail address, job title, company name, telephone number, tax ID or other information that is sufficiently specific to identify you. We do not collect any payment information on individuals as this is only held through PayPal, Paddle (our merchant of record) or other such payment website.</p>
+<p class="style_7">Non-personal data we collect may include browser type, IP address or the URL of the previous web site visits, location data, device cookie and identification, operating system and platform and other identifying information required for your device to communicate with our website, products or services.</p>
+<p class="style_7">Non-personal data generally does not contain any personal data, however in some cases non-personal data can be stored together with your personal data in such a way that non-personal data could be considered to be personal data. If this occurs we will process it in accordance with this privacy notice.</p>
+<p class="style_7">We do not collect any data in “Special Categories” which include but are not limited to race or ethnicity, religious or philosophical beliefs, sexual orientation, genetic or biometric data.</p>
+<h4 class="style_6">3.How we use your personal data</h4>
+<p class="style_7">Under the GDPR we must always have a lawful basis for using personal data. This may be because the data is necessary for our performance of a contract with you, because you have consented to our use of your personal data, or because it is in our legitimate business interests to use it. Your personal data will be used for the following purposes:</p>
+<ul class="note-privcy">
+<li>Providing and managing your account (legal basis: contractual)</li>
+<li>Supplying our products and services to you. Your personal details are required in order for us to enter into a contract with you (legal basis: contractual)</li>
+<li>Personalising, improving and tailoring or products and services for you (legal basis: legitimate interest)</li>
+<li>Communicating with you. This may include responding to emails or calls from you (legal basis: contractual and legitimate interest)</li>
+<li>Supplying you with information by email that you have opted-in to receive. You may unsubscribe or opt-out at any time by contacting us or by clicking the unsubscribe link in our emails (legal basis: legitimate interest)</li>
+<li>Finding and preventing fraud and responding to trust and safety issues that may arise (legal basis: legitimate interest)</li>
+<li>For compliance purposes, including our Terms of Service or other legal rights, or as may be required by applicable laws and regulations or requested by any judicial process or governmental agency (legal basis: legitimate interest and compliance with legal obligations)</li>
+<li>For other purposes for which we provide specific notice at the time the information is collected (legal basis: contractual and legitimate interest)</li>
+</ul>
+<p class="style_7">With your permission and/or where permitted by law, we may also use your personal data for marketing purposes, which may include contacting you by email with information, news and updates on our products and services. We will never send you any unlawful marketing or spam. You will always have the opportunity to opt-out.</p>
+<p class="style_7">For users of our products and services, some products and services may require certain information we have collected to ensure that those products and services can operate. Such personal data may also be used to ensure that future product and service developments can be customised or otherwise modified for your specific use.</p>
+<h4 class="style_6">4.Storing your personal data</h4>
+<p class="style_7">Once we have received your personal data we will use reasonable and necessary procedures and security features to try and prevent unauthorised access. We will limit who can access your personal data to those individuals who need to see it and who are subject to a duty of confidentiality. If we become aware of a data breach we will notify the Information Commissioner’s Office. If we believe that the data breach is serious, we may notify you in accordance with our legal requirements.</p>
+<p class="style_7">We store and process your personal data on third-party hosted servers located in the UK, the USA and/or the European Economic Area. We use industry standard mechanisms to protect the collected personal data, and all such data is stored in protected databases with both physical and software-based access controls provided by our hosting provider.</p>
+<p class="style_7">If we transfer your personal data out of the UK/USA/EEA, we will only do so when necessary and will ensure a similar degree of protection is afforded to it. By using our products and services you give us consent to store, process and transfer your personal data outside of your country of residence to our third-party hosted servers. If you object to your personal data being stored, processed or transferred in this way, please do not use our products or services.</p>
+<p class="style_7">We will only retain your personal data for as long as necessary to fulfil the purposes we collected it for. In some circumstance we may store your personal data for longer periods of time, for instance where we are required to do so in accordance with legal, regulatory, tax, accounting or necessary technical requirements.</p>
+<h4 class="style_6">5.Disclosure of your personal data</h4>
+<p class="style_7">We will maintain the confidentiality of all personal data, and only use it for the purpose for which it has been expressly collected. By entering information and sending it to us, you accept that all such information may be used for sales and marketing purposes only by us. We do not give, sell of otherwise pass on any personal or non-personal data to any third party.</p>
+<p class="style_7">We will only disclose your personal data (i) when required by law or by a relevant regulator or other competent authority, (ii) if the disclosure is reasonable necessary for the purposes of detecting and preventing fraud or security breaches, or (iii) if the disclosure is reasonably necessary to enforce our service agreements and contracts.</p>
+<h4 class="style_6">6.Your rights</h4>
+<p class="style_7">Your principal rights under data protection law are:</p>
+<ul class="note-privcy">
+<li>the right to access – you can ask for copies of your personal data;</li>
+<li>the right to rectification – you can ask us to rectify inaccurate personal data and to complete incomplete personal data;</li>
+<li>the right to erasure – you can ask us to delete your personal data;</li>
+<li>the right to restrict processing – you can ask us to restrict the processing of your personal data;</li>
+<li>the right to object to processing – you can object to the processing of your personal data;</li>
+<li>the right to data portability – you can ask that we transfer your personal data to another organisation or to you;</li>
+<li>the right to complain to a supervisory authority – you can complain about our processing of your personal data; and</li>
+<li>the right to withdraw consent – to the extent that the legal basis of our processing of your personal data is consent, you can withdraw that consent.</li>
+</ul>
+<p class="style_7">These rights are subject to certain limitations and exceptions.</p>
+<h4 class="style_6">7.Data security</h4>
+<p class="style_7">We use industry standard security mechanisms to protect the personal and non-personal data we collect and hold. All such data is stored in protected databases located behind firewalls and with both physical and software-based access controls provided by our hosting service providers.</p>
+<h4 class="style_6">8.Cookies</h4>
+<p class="style_7">Like other organisations, we may collect data about how you use our website and for statistical analysis by placing cookies on your computer. If you do not wish to have cookies placed on your computer, you can set your browser to refuse all or some cookies, or to alert you when a website sets or accesses cookies. Further information can be found in our Cookie Policy.</p>
+       </div>
+         </div>
+     </div>
+
+
+        
+    </div>
+</section>
+
+</div>
+
+
+<?php include("footer.php"); ?>
