cbee0c6d09b6c49346c90f82cbce173eee21f92b
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 10:41:14 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/ci-business-process-management.php
@@ -0,0 +1,200 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">CI &amp; Business Process Management&ZeroWidthSpace;</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">Stop putting your Operational Excellence program on hold!</h2>
+                    
+                                            <div class="content">Start using Presto P-D-C-A today. Start free! No credit card required!
+
+</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/first-task-list-1.webp" alt="ci &amp; business process management&ZeroWidthSpace;">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2791-o1" lang="en-US" dir="ltr" data-wpcf7-id="2791">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/ci-business-process-management/#wpcf7-f2791-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2791" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2791-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA7tmuVep-T-fRBnQwnrNWq38qjMUL8ZCLLJUlsqZj0gSPIDSplaRElHd4xuD0J_T6CfzFJJcrBQ5WxNwxVwPTZnGpPw7u7owM3uWpS6YB4OoMlrviUJQGE_614ucdkyhvre_qxkpGEegLhRQgYfbVZJfweHtrU1W0Cn0zgUnyU4-c5EFnRrSnp5YQuCWM4hfJ4T8Oo4UdNuJvQxcrc0lFnPBKfWGMLwh7xuu1OPthGyTKy362wtJcvJlVEioBnfRh3udDtszv1ZEu6uRq09knIo1CQrBgbVY54JC0ST6Svq0u7RruWEjjVWtGN4AaTJmwONHERSKt7b8mQsf6qqtpeIMGd0QoBj74opAAZ1cn9eBWrsKaJ3ZSuhiVT7LLeNgs0wOdbP_LsJOlKZjXSWckovivYynuhcAt8uqUWa7izZLrdY-_SnlZnVsyv0A321ddjfK4-HEh4AarC03TavD1taanYFqzeuFe4NtLGMk0lN1Q95XQecZGfA2z5mp8hTz2XZ1sQyORKJlvLWWlCUXJB8UVemjWZcOJO0G9CnaSbtPquLnmr0JFbgK-Px0p5l4BGkAhMPjHq9Tcdtncz1ibYVeD172_jmkoerGH5cT4xE9aaVgvq9GBnMQspctKUVOrYwP6VN0DBm0S4reai5B9ppP3tmvSemPtqWiaqvIhNwefJW5ZWFs25nsiDCZn3YTOyjiy12DYYhaQd1nARI8EUQVtuQoWjJ2IvkrUN6zo3aybw8Js6xw8voKegkdpAcNDBbVSbEQ1RxqFKk1y7m46HHKtT1zJoB9cqIKD4bpfedIXnZg5kH6lQZroiWYKJxD3jYLQOqbJ6RdH7PYT99fjX-htNPCjjGmI_v-KS0bbUctDZuyIOnREh936pRqRUkvFkZyrP1dF-_lyYngGjxotR5cUZcRN4SQh3KhUE9zd4tUkaI-KGKwSqFTMbD08tLyAgDihaGAoSnQiXsXMO6DN-5w43VFYXmlyw-cdww7lfTrpF5Qv2aOl6d7smdYljMkCQK1X2BRsKZ_Fmu3196YKQHMIReczDaYMyFbk4n3fHinjKKB5x-ygrvRrFd3jjCvvxiCPfEYH-C3V0w8kiWKVnDRWWn8_OBFy22P1Srv4xwk7vm7q0dyyILjAOPYk3hfk15Uws_8ULANk0E38E79oMAf5ywEeUo5amMZwJAHzrVjD3uxhhnNwi9NSadMJBHiuVBc48w0PPvHOe8TC8EpSJ5S2bJfrp0UzEKmTVd-t4nSKQs56jVPylQgbIQ24AVyF4LfOTxHEBTjO2tfVj2l4E8E8kKX-W59MmLjFAOv60tUW1auV2jTnd8u_VNvlYfaaVi_AfGFpCFCjajoh1_SCFQYCpa4VQWNV1jtLOidbCcGeQhWFOy6k8eMq05oCA668CbysQ2Xu8C04SMT_NttWirHBPcsRNJGEWC0MU-S6Q8LtJ0Ax_GkNJK3slUaW5jXVNHMHD6xihtEcBD9LS85nMKMLEYwxLGPKOWSbOxecrmXHon78Mw2u-V3QZvT2YTq3WFwJHo57AssT0tRlVZiHnxfx8MU_gQp2UYKrBTw9l3pXb5hmNx2B8_IaFckhwWmmmyerp0QZ_WCrqujWZTf4b9mJp-5l7OwoDnGaLqbXUfjOqaDMfbdK6HMCj_laI5fbdESkAv2KN8cm6mwWUSC-BK9ZXjjSMlnm79h-vmrKIoxkU3TELtI1eNoYD4dWudOQw7CbPz3p5EVgoLW2BnU_SEVSlOmYFwqKlXc2v3xJ3rgg6Okx1p5wEMgsYijUj7-f5WH0rjdLg0pbkbUuXt2-kY3-J9QCpoxGvzj5xxWzC3qvkQc3H7AX9axkW7w_bQjU9Fdk2JHWJ4VhGjtV5PQguczBKlWF4PyTu5T3QUnrJSWUkF-4ijCD7qORGyp2yamDKeMB3URurM04eOmNCQ2Q0SYjNxHEtGMDmZmOHXrV6oHJZBZAs5HM6LgD6k3r7lvtC3iKQEUtI7BX4I9Wrb2TI_5SRPhlErnxnMH-nlZlcPcGYL0pJ4m8p45rLG0Ttg0qwN5fDYKfdQ4hBQLP6Avu8EWuUe34u0dLJVgny5m4d_nToJnKJO-Xe9AtqcRGFB8wkABbbN9TD6_3_a8UWnb3EqkPlAFMnLTxqknzjbaw3XlDCQFCJuPOoqiHQMQr23eeDe6DOKP3Vykl0BoxsJjBrpPMeRWmKLdoyudVIVBApYL9XOLn1u5-Eei_E1cPlIbbgjpOzD8-zdyzVp7zPkzqv0eFgi3k4uZ15kr2bJTuzuzEq2pJGt066cwjbj0BhH_LUhxQv6"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="field-guide">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                    <h2 class="title">Is 2025 your year for continuous improvement?</h2>
+
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>
+                            We are thrilled to make 2025 our year of automation, introducing to you our all-in-one
+                            operational excellence and continuous improvement platform designed to revolutionize the way
+                            you manage your business. Presto P-D-C-A offers sustainability in key areas including
+                            project management, process management, strategy management, talent management, and document
+                            management. Complete with a mobile app for task, ticket, and issue management and now
+                            integrated with all major email providers and document management systems, business leaders
+                            like you can tackle the top pain points in increasing efficiency, improving employee
+                            engagement, and enhancing business execution. Plus, with our free plan option available, you
+                            can experience the power of Presto P-D-C-A at no cost. Dive in to discover how Presto
+                            P-D-C-A can empower your organization to thrive in today’s dynamic business landscape.
+                        </p>
+
+                        <h2 class="icon-list-title">This is for you if you suffer:</h2>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>
+                                        Lack of visibility: Difficulty in gaining real-time insights into project and
+                                        process performance
+                                    </li>
+                                    <li>
+                                        Inefficient processes: Manual and time-consuming processes leading to wasted
+                                        resources and delayed projects
+                                    </li>
+                                    <li>
+                                        Poor project management &amp; prioritization: Projects often exceed deadlines
+                                        and budgets due to inadequate planning and tracking
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>
+                                        Employee disengagement: Low morale and disengaged employees impacting
+                                        productivity and innovation
+                                    </li>
+                                    <li>
+                                        Knowledge loss: Risk of losing critical knowledge when key employees leave or
+                                        retire.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
