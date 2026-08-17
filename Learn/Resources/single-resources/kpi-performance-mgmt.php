e7967dae18ecc9e835a8a73da2937615751b3d37
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 11:13:11 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/kpi-performance-mgmt.php
@@ -0,0 +1,189 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">KPI &amp; Performance Mgmt&ZeroWidthSpace;</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">You will never have your KPIs working for you until you learn this</h2>
+                    
+                                            <div class="content">(and other tips for the growth-focused leader)
+
+</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/kpi_tactical_field_guide.webp" alt="kpi strategy execution">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2796-o1" lang="en-US" dir="ltr" data-wpcf7-id="2796">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/kpi-performance-mgmt/#wpcf7-f2796-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2796" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2796-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA4hTpAYJ_ikO_4V_qcd8A2GVBqoT8jzMy5b1HOh2U6hoNsiorqbeFzjoBvuNV4ZijEMC3IVEVMl83fPxz6LgCewJe1c20lAZoypofDStI_aH26RfQZrdYDV_KK3dweXGcLH6CYXGRs_Fo2-Ei5o05fwxZH8qPD7W3D6aI8YTgYwb-0U051gsncAV2cIyN7k_H5WaOneZ6sBH2860J7rQ4wbtmF254919Wv1EM0H4O8hdux1RAgx7L3kFh0mCqD4yjDk7O1tWNxMEVw8K2l1zu39Af0tW2kWOel2FkBKwZ1ZsTrzESQGp_sRz1DMSCOgXaUscuvllMIhiOah8W0ikaHs2HfOQ7dmoE4vV1SKpSY6EJr_lU3Roo40bFkvqIKDK-7Dciz8i0eSGXN1YZxlSN9jndXaUQTleVVnc7DZ5OKJ6-eMPn7Knm1IvqKFniUnrkiurGIByK_MY8XXkRMlmFc0kwAQI1lNMYoZV12T6OPm_d7uGs1PdSIpXtSBi4hx_N3darfUecmgMQgm0V7SHNprs8KIkLRp-gTu-yjahHgYVg_hwdQC6bj99ssHFg1D48LCMroiMPgMBI7O1zsLw7uCJKbW2jlcR-SdYryCfuW13d-5LbKMOpuV_CxDSbZjgZTFBLfymj2uC72TIIwH320RbfjxjSpffdwhtnoNXZxuJDqewOAxooqEH-vOz2dt5QEdK7g7RO2_QXPc_-dlGRE9-bU8b0Gflcp0-GA9Ez3ejmpmrZ-l-yGEJIBtemcgiQorsUaeWWzZpxt0wUlduncRtBbE0d8e5xBRUda1UIS_F1qqqmT7IhZz9H2Wsl4iH2Ee9-KQ2_TCCuQ1QgrN0Sd_o953NBeOlR8S9OOGV04DZ-X97xK0RI0x4b6a_XsnFut5yDA35WxdU_GH3pualHUa0WUAwdmPvYrcuq_G_TqPGq3K9UkpRzxdwe69r7XKr65qwa0rXbpr0W3vazy1MDHQfAy_3t8lZTMu-repV3XnGk90F6ffps_0rS3OeWKyHnQohQf3wXAYsGvB8VGI2-QH4hn86_7G9QPa7M4iuJSoURRuPhBuMmrg3DqpAftyVcUvM2cRnDYIAiyT9YYSJy76vGWOwwu_3n0AI0U3BOZ26xAWLf26Oh0RlbTDfGuWexsqgUk9M4f1k8W6wsCqAu1bBAhfEegQfs1uImxH75N5jcxJ69_SxH4sQ6HGRFsIIrO2qe5slpzOSJ3vfXFCdFZAy9zLOw7FclQZMzwedX8B1ZqWYRSXqJEeikC8uT0LIVBHyJ5DU60z5SJA4tXZdlB7Be6tsRMOVZn2gS5yzP71UzEZ9yUnvJZ0rrYCTvh4n46giwAPNT4pdNoOb2Zs9dj_ZMiohNG974DnD2UDsvmFR39_ETcph1ScYoZL2Nx0hIDpX9xy4zJbsB84ho3pyL_U2dg3-1glYIFHp6uAB1ZkxKdtBfP3PPQMqtkCF1K0fEezcVVB-QSldUlS4CuG8Q-BWLFl4QnX48zwxFeLvFJoalyrRCG13jhDcz8Izrx8A9ydVJNroguhgev71c64dcRkAnSmkPfOhMM8ks0wPzi6TRcmaqiNoc7WWnFSFsLaK25DGWKDOlDJBXiF1faHzAamJ6X_erph42HUnZ9XlDzZ9UaTWlyexXHW8qPnZ_k6Ietm7zAZ9YyNDRdVw_i8IzP2WUkv0SI2sigeJNbX7ZMWdcX1Ty0lVOclnkCkLdRvmG54cZMb2UYFaotOdJv4OJKlCw9re3oCvy5GC8Yn8xKyySIKISFSOVlZcHChgOB_sHQKnuPiXZRl9f1-h0pjRTbhhFplMLz_CadM_kSe9PBnR1h49O9Ub9wUCDuMlLQoOyYnpipuJVtAByi82pfUtNva9p98TDrP-GN6WQJpiBrlf9VslkhJGo1wbRs46h-jcAAeTVj1BFVvdkawGMSilJaQQZxJJJwlu-ngoSuXxhxAm5qQbeB950bOfeKHnX4XPGUYz4_wkCXnuiiZxTqmz6K6lxcq1e2XUll07etRXIkrMKwLU-7k8sumqXI9ad15me05IT__dJTcRUwZOYHKf6-DIz6SIQ36MI_mSp1UkpvHQgp-56IdNC0_5I8awHhbHunacXPFy9LMdNpa5kTCw2NjThZBYYy-O9AL9dGMU7tYXWY8IpyZZruXG_pSO0_x0YI6Qr5TJHPGf1OFaHun-yNIqr4wyjFTacsnBG5f3bSeZZswOmWUCylRD3yQfgrfLplEoBbStOFKhuYIViP9HfXTOhZAVQ"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<section class="field-guide">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                    <h2 class="title">FREE JOB AID</h2>
+
+                    <h4>From KPI confusion to strategy execution excellence</h4>
+
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>
+                            Does 71% of your staff not understand your strategy? Yes according to HBR. In this compiled
+                            publication our team has put together our turn-key methodology you can plug-and-play into
+                            your business’s KPI management program. By putting the right KPIs on the table in the right
+                            moments, you will inspire timely and precise commitment delivery to get the most out of your
+                            staff, vendors and clients.
+                        </p>
+
+                        <h2 class="icon-list-title">You may need this if:</h2>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>Your management reports are not effective</li>
+                                    <li>You are not proactive in removing performance obstacles</li>
+                                    <li>Sense of ownership &amp; sense of urgency among staff is mediocre</li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>
+                                        Your management team doesn't use the Balanced Scorecard methodology effectively
+                                    </li>
+                                    <li>
+                                        Your team leaders don't understand the relationship between lead and lag KPIs
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
