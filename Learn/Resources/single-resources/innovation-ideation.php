35792bd28c05eed485693161495ac66e64bf80e0
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 10:37:33 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/innovation-ideation.php
@@ -0,0 +1,193 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">Innovation &amp; Ideation</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">Idea Cultivation &amp; Process Improvement Execution Is Easier Than You Think</h2>
+                    
+                                            <div class="content">Your 13-step do-it-yourself checklist
+
+</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/13-step-innovation-toolkit.webp" alt="13 step innovation toolkit">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2790-o1" lang="en-US" dir="ltr" data-wpcf7-id="2790">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/innovation-ideation/#wpcf7-f2790-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2790" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2790-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA7jdu5u3ncLsHwqS1AFJctjAFpaV8KdMi_kqjdN9QT3KIYbSqf2Dk8_FZXdTz3bl3b2BLEz38SbH-_6MKZBP2UkhL2HyNWnkpBzHM8nZ_N2xCHbvulqFrlfKWf1o9SxNwUwPaIPPCngnVTKZi061v4cIYCI-cr49XAf6QF-EYex-Sxw0WT375BQZ2idavUx7LHKeTwRfEaElHjTBCJ2QGhD3whpoZvaPRZ3kYe1ZKpDoqz5UneI42OAaAhlKcKHwWb3lQVt1oC6_kyP_yTo2q7CkcGwnDNph3dLDeKPgxKCkaBc9iv0vQ3_WlR_J6LxK4lPUzrJavEJPqRyZXP-WNMEPCq5dF5wXAGTBFVTcatAoibWE7dIalzepcuyEWqGKuwai3RrsNvtwfI27zaVt5dI4vKVKpnEZDErAKq5zHhbq6moURyhakpuyVAq5BSPe-EBOhxcW6ZkOTor8ao0jYbO1TY8knHlZ5xcE-gy4oXfxYqQbIUmxN7VIsiQ4hcgfKeSVh086YtHhd2IQm3C_ep6eHrZ9npMzU9jBSZ7Oh-WIBuUPu3qEy78bIzohv3-9yGG9xGNaUvPzMn3PYuieDpgWE64-JKJWRFhiMg-_SoAcDmeMZ3sLpUqYmilxDr_XveLItfv4C52iYbpiAt00_R9PK9CfN7QRGjcnJsjB8MSI1fGtt0Dnj0WW3JuC_hynyYnlJwr_V8oUKgrgX9Je74LvVCXMnpiFQcQlfrcY1AtB7wGfs4Ww8Erl-exJ76J_ZtSyOKYvt9eVllUAkpRwbkhhM-DX8i5sHVVDDUPYD8lqotzpvyb98KVexssY_zDldITZTukcMMVYdXil6UtoxVbRFkQtUpTbL1xfruXJp9HKCYjWZCqIHD6rUlZ695ngvLL2d3Rtkj5zSPahTxUoc26_aUxzNsqb_xJMuYQVKXF6H1ZLs6HfS_jsBhqefVv_mg_YKdhmWT9PE1oJtLjlg1D-fdT-TJt6HypC3UZ7rY-3rP0-URqDkFwLqcxt1FTL0g9SyLuzjypvYtIfhRrA88eYjsR8hUEvtlXA1Xfl6s_TM7guf1XoimWj7m0UlgSfU3YNS23-YOA5M8GFJiRr4BOvUiLpvy0AbLO0dE_Cu6sAbLLLXt89n9JFQ--LctSlDtk-J-4gYU0UWPguLnV6TGcx6W5zAWhzpbPNA6uokPsybfTUHTA9k45vyJ_79HdmTH9NV1YUn3dJHH2fkCB4JrNwklUPCDBgsyoIvj0wmQyiJNWvYdWE1k-UZywFguh1UjZbmjl_azjmnSvVYZmT_C03CPEMOpnEGERKeyqsTorN0XyAuHBj-pwWRancKGWeu1j-_vncEabJLkfIKPutOhSpPJasSbVojpUKamvYLsZJKVSJjiF6la0X_F3VH4MXtiec-mK39Ka5zWi38oIywTv1yuyybAskttNLCDhX1YES7-Pnr-t7LDlRconDiK0uVjLWsd4Cw7CUWlOv6_hrPsyAbhZ2kqsRsf3X8796G3nLCwi2Bl7KXgCxtJqb0jJet-myQujE9NN4ktY-UmEm3Dw8nBrdWYtoBNCNeeYAXgvc8QyuRwxe-ldtWQB0EfJJdhb5mKQQEjGeuefRJxgYFWO6PpNFfr7H1yhHXbhLTStD9IcaRxQWw7s1_MEO8doh1WFOteS0-bZOjoXEhRwry7rl3BXBDkoIc-SUPT8TFE0PFp6g4CW7Mp750gt8X_hZY6ddC19drZcPytwQqH_VZG1lpa16o0EZ3B5Jk0TH1sU-UFc0fcuZGytvBntLC34uWF-wzRM-D3jQ2VjTU3Fuxrv5xtVPJg2EL2mvu_5UdZ4mtKsoEcD6lWgb2uWBhDB5IrJ2Ve7J5VcHiKGJEnqhPpd-AGnJUEAyFwj3HAZJh_G8H1u3aQC_IOdzdFxDs83sXDLroHndvOlNifq8yD8WEdQv8omdl_W14oZZSI7d2cw33yGH3Ftn8Qb-02CJNjGCF5zsSmvSVs8JEbMoKXR3zygqZJnyztXZN0cKKpeYohbM_eWHuKHtqBIPuEvVsVcgciRB12fmWluT7Ale2SjSBZ9yWOihquuGaO6xtES4IisUc-czt1BPhg1nidqOUAjjBVrPlXEfBozYoAfEpuEbWWTVJAtfJSRRNM15iSixasIVxTwnA0VT8or0mZBNRI6Yjhy_Oz34sXaE1I8UlO1ePiiRRfXXpqDWE0c-NRvVk1vm302Sgm4Qj5l4p2cNQz6f_0R7OgoLV1A7duVfGmf-hl2WAQuiQ"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="field-guide">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                    <h2 class="title">FREE CHECKLIST</h2>
+
+                    <h4>
+                        For CEOs looking to build a company-wide culture based on innovation and continuous improvement
+                    </h4>
+
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>
+                            Challenging The Status Quo Safely Is Easier Than You Think. Find out what key ingredients
+                            your company needs to induce a culture of innovation and ongoing daily staff-driven
+                            improvements.
+                        </p>
+
+                        <h2 class="icon-list-title">In this exclusive report we reveal:</h2>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>
+                                        The #1 reason CEOs can't seem to execute swiftly in business innovation no
+                                        matter how hard they try
+                                    </li>
+                                    <li>
+                                        Why you should never trust people who say 'Now is not the time' and what you
+                                        should do instead
+                                    </li>
+                                    <li>
+                                        The honest truth about business transparency and why it can be so much easier
+                                        than ever imagined
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>
+                                        How to leverage lean methodologies to create an innovation-focused work culture
+                                    </li>
+                                    <li>and much more</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
