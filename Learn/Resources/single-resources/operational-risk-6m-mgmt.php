49652bddaa7752f7bed70670c400b9fc1d16c7d3
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 11:34:05 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/operational-risk-6m-mgmt.php
@@ -0,0 +1,166 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">Operational Risk &amp; 6M Mgmt&ZeroWidthSpace;</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">Proactive Pharma Risk Management: Mastering the 6M Framework</h2>
+                    
+                                            <div class="content">Discover how teams shift from reactive firefighting to proactive problem-solving with Presto PDCA
+
+</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/6M-pharma2.webp" alt="Operational Risk &amp; 6M mgmt&ZeroWidthSpace;">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2793-o1" lang="en-US" dir="ltr" data-wpcf7-id="2793">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/operational-risk-6m-mgmt/#wpcf7-f2793-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2793" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2793-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA6NuEZyqB4PRV8o9_gqSwNFmipJNUEhGbYK8N-_XPsqGQ1hCP_-nClUM2l8g6W_CQ3TWuv_XXYLxYZMJ_VZenHMtkXpd9yU27lVaSzJESaxoaje9suSo62mFhf5O3jR_0fqJBPFIm7mxUoQcmk-i3FOzOA-WBx6ceXaOhASmJYBRq-tdZOHXIm_szAkv_xO5eRl0EW_XEIj2T4eeplUN6iEZNha1vgRIZ6G07LcpQcMEhyoAeRTGspkOTKtjk7FQjnIWhZ6x2D4kD7fsgRceNECHGR7L6W_Xk2zzlqvFYNvewiOq0iJSTInAQKwt5QTeyDv5HkCS1qIxmWxKKw9FgUFYgxYd98VNfFvEDiMu229MT3fNsJpwMbMjolE2CyjnZkqtEwBvsxvY0-8rbv6dSzHMNHGQw2J353irfjC8_JfSX0yUWqhC0w6o5SUG_oNd8lt2nSEL9QF4gF8QIhBZ_B4wB6AzGzBJp42_dO1ByayoZosx3UQCxyHDYiDpijaet6IVgdGWFJnS2FVYy21E_6O0t1RLagK7JlqioeUtQR6vH9QLGNVSrUxUK_sVb5kyIZggdJCY3NiAdHdUOPD-mHdiWCTlMBnfBXBwQPNkreberF5qWmL2P6zvR-D_BfRwDcGsdgl4LE101VYbuzCr6-FXnRNyy72UU7pEGxcWkJ2kVGK-cQ50k57-gelcn2QLvgFSRo8RF9hIAVJ3bFyyYdq6vM60pLgmXusbY5ObuQg_Q6-5o3Au9aFCNdN_z7eQ6pXE9364t2VmAoYM63suD1Ips-dziaeC-iKRMK3waH7_uXbrXd6djp1fjxHqmDfzKUOcVvcVa7QlOv-KsgkCUS9xo_KjOxhfcMR8EqMeaQwCOmrr84KkptrA-VWDyGc31pORAdsrn1HrtStXXBF7SXzSN6dZQCOPJcN9m4cqJJPIV3XSqvQWyz4xHxw0h1UNFFHdhzZN1sehODfOQPg5bpGnjV9fOwIhmncA7FjXc4Sci4wSHEIdPK6x-zzALce3wYB_9UzBu1XhmvVIk0o8ErWFPBJaDfR3R_EpLzPK9bJH7dNa1-HU58K8dQYbBoUw3_gbnCAqjdF9Z0ad4dHvceTls6bPDsC-l12p4t3E2Jb3vdquDen8YaXFt1eUUKdtf4HsrESO1_ZBzS2UlXoTx7Ks6XBRWPG1kzyH9ho0qcm2X7Ill5-RT803XlgdtscrTBYuNw-HGCsdfmavkWka-Nz198eXEHxzNV236eF91cFqNC1_j8iXhwavjZSB1TdlTcvcEfTrxqu02irk2U23eFO79gCJ4ICnZ1jL60Lwh4j64lDB3E7tumHUy1Ivs4QyurjPXOIi-7_o2sAbkpDP9yxCimoK6g-94RgXRF3BUS_iTVw02dD5Q4jfP5ghPvPL2dFeJdsI-rG40r166f2-SRmcyd_qZDVLIngyh82PhIT1iezKAlM4ekrUnqC_nZZLWYodUC1k_NBHDRAyxF3boiGuInhwCyjIjPoP6-O0QTQm12JrIZ52ryeKLTYkYA-DNHqsWu87yTZEEL8dpZZkIB-gdHCixmkgAppnSxA6K9RvkMBatk-GDsZpUcL_WEzm5akSvdhNc-Cl84WGlkW21OA8Dg4gBLfOhAwI2KKw6lWw1RV0uPeFAbRdXQ2ztU4y_10N-6gGZxxJgF-dFhoQzjKGk1Hp0KOxaa8PbMIdU3h_bBJbKODco2YsuKjQTRTAAi6Fj-Dx9ALTVGdhSlfwQkMJV-ZJqzRNrbjd-5eUTtYPm3_gD4FUcEbZHQ3pJ28M7BM_KpEWyGUBV_X-Ws0nKuH5DHsOaUQnDydKxrv3iHDGlrBuJys0pfZU6osbtvJcGAllLSEpCm2XMImDcSdSBbm7kXNmns2sG20HSCKGj7yDKAmFfKUTOZW5LAGphBL0k6t7AT_B1L8IV0gAc3e5f51GYsY176YMnS4jHjNogRVIikQsqshYSGGcDQOH8pexHAdT0ak26oE4wy_DSVmHHKpWLyqFgmiPH3z4XBtVkX9mzqGT2c3-GDkV6l_rUzpM9akowdKXPXpA0UJlpt3UQ00NXjEoofq4VSLV-Q-Dv9B0LHSaEXPIXVMJnWJsA01RWTYk4reWI9H8eJC19rVMO496YFHzEpF3Lgo2A3zXZMVhAklQX2KI8MV-UD9WLaPczPj64p2OZZGv0gzOXT1nt4IAx38fbyyUbXRhv2oP9idOvGFgdzm-zQ3wlOIlpTx6yA8MqgdoGI0SvDVEvYb9XaK4FEYdw"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<section class="field-guide">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>
+                            Pharmaceutical organizations operate in one of the most regulated and high-stakes
+                            industries. To stay ahead of production challenges, compliance requirements, and quality
+                            targets, you need more than just quick fixes-you need a proactive approach. In this recently
+                            hosted webinar, we explore the 6M risk management discipline (Manpower, Methods, Machines,
+                            Materials, Measurement, and Mother Nature) and demonstrate how it can be seamlessly
+                            integrated into daily operations using Presto PDCA. Below, you’ll find the entire 45-minute
+                            session split into two parts, featuring expert insights and a hands-on demo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
