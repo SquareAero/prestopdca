2d3f62e62ec89a5bbe2505ca3b654a323bf2eaed
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 11:26:18 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/pmo-execution.php
@@ -0,0 +1,180 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">PMO Execution&ZeroWidthSpace;</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">Swifter Execution From Your Staff, Vendors &amp; Clients!</h2>
+                    
+                                            <div class="content">Administrative burden is killing your ability to execute. Plan, run and govern your projects and everyday work using this simple P-D-C-A / R-A-C-I framework!
+
+</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/PMO_PDCA-1.webp" alt="PMO execution system">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2795-o1" lang="en-US" dir="ltr" data-wpcf7-id="2795">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/pmo-execution/#wpcf7-f2795-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2795" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2795-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA7xEcKwCuh020UcnC792RMfq-RTMRsbFFILIk1y0hoqPbNaWJKRg7GqYcgEL6fXhkwok2v5pc7kQ3rKL3S9QjmqTZ2ND54-O4nyRx2kJcPt1vcIMaplOAxqwSTfP2YloPuOAU9z6yyLtPyfJM9Rsy4MqgA-vA2WT_JOLepevldQt-v02Ki7VQCa0IEdJsCdBCagVkKgel789_PGJbwb88iDN0KvQsnhT0Ws448XQiUPEslK89hn3-kUYLtbCRrEzKbuQYKCd4NshT6acTbHMo-VPSQVCwA4X7uKFAFfhPPMj2i6AHgX0IWa6sXq6JVeDCPd6h1I0qYuovLTB5bixWaSIT6nhG7AZTP97GOMZ1DuPzM1vJ4L0Lu5G7vxOhyyzzv-Bm7k9P6_cnRELzJ7mnkwCsncxmIbsH-FOIA8BdTCbjfhTi5METZG0cWryBYIoF0yTLaCjPcLcWM9zD0hSyXFbgl698_YyRUUJR8T4mAWEjOakT67q9UpV_KFe4SfWXKhViYTTqht_kYCmvcjaIHw4peIDzF0qIoLs11rIIxaUx700oIjfhGD11PixYOQDK4SlyF8NDHQEn9tIVkbe4bALs2ndJuCNRmrsfu6HvtEfuJhtguT5I6pmOa1ljVAUo4hjgJ9Q6XBJfupiPsgyQaWHX4GK99Ym3tQn8Yi1OSXDcG7YqjKHUHzkG_lbx7YQazQXQ87LI6ACPWxlWEJkgdyRCK34CTGtAnk5Bajj9q5Xlr85n5AzBpZoXnpB9iBm_z46-gH59dbvo6jX1-CCGPPF0TCyLGm_snp9eFwVz_NJnxmfu89I0uWRSDoCVuIIhowMZrPncMWW_N848Q-O0wGT_s8ZdOQ8nlWnhhQCs60kdtxBOLCZE7X9r_qQ9TcQwl_Yf0bPhjeCcoE3bsSn9JxvcqQlWVOOLdWff7v2hJh3B2fe_R_NO7eiRHmPohYwvhyye-FwP8uRut7eFhzE-n0TKZgFHGw3i5DH9pty3hb7uUcg3g3NMQckxcTb9lpe0FGlbAEzdX-cX-FkntbcH_Yoc7N4PY6t2ReJ9G_IlzQmnDwRDbHfBxtvGf9sdEDz-Pjb8uho5p2rUbg4DW201vuCUrXWy9TQ6F0CRRFuHHAW_Y3nMfYbpuIc-QEoUq3NTFjIOjNtPKH25Wcng9Sfz9Mxw5PIGWlcqb4ftgqHWwzAmwaRVTvgE-7ehtGhwVSW7aF0NnSe8vQ5t5uF7NH2b7eC1nyaxHLtZkYoR8TpVbFuaYOXuPJcCwXIWEjo94NqS0EgzsO8mZjfGQFA78c1BYCV0yf0bpff0WX2aVpHMvk-Gq6dMuiqIFmVNikWmpxx1w6nb_BBdVMbsnPxMl_8eLgPOy6LRDXmuQDdsLpZGcidVaij8TyOzHpiawAv5CC1PIwu_WbBDFUGvxJf5A6_lxShHBBA3hQ-204bwVrzEpcrhA-iY-IP6qm37c0IuTcvwOsYCAluu-GuV0IJSLPIO_ra2lJT9F6MJzfjPbwrMKrer5sEvQjQVOIXVksEr9koHEoJRZcupz39XLc6tOHXEjOPwutK6eRJ7Vo2mnGc_7eSL5vPzK39-2bKbVPRJan0uqrDdTio1BT0wKj9CEY-gdvEFNG4-E5KAJH8hBAr6ZvQiK3_fqgAJr4r5MEGrLUwY4MbEl-HV_Vleymoyz7XWWd34uOL8FsNt1wB6CD4tOOiYgFdJtxb6gix1gKe3mp1gA9wV67B2xC6CKI0fY6tEHjeq-ZLSs_zD-XDm5H6bJRWNnnzyhn3NZXJ4yNhMBPbzBKC5O4GHI5DDp_w1SQ5r493RybKpO5SWFYFAQm35WoVDI4VVjkuQPYWGZ53ewlIHOYCUo2J8bAb4zWpmbuwGVvOazkpPCDJtA9L-_rHb8lHFpqom6e5oE4QFY067zLp8mNxKDha3zaMrHLjv_I7vNT_3qgztSAf72NKfOVxEThgmaW8pt8b5tGpnLmARPP1x3uVrJvlxWQtTX1JUE39R_TPdmX3Gqp7BKOoGht4H1O6jc2zI14XCWEmJrgjpo2jaNGXsxwXOgJHO7TRlhW7s36uayBFwDD6SrKsuor2_zwiGUEcnVqrvTVEM6jZgw_VPRs9o7bP7UxREvYE6G-QgVFulOqP-micBhFaL7XmQWtv7dHQh_rq2SfS7IgGeuqBnBeosH6_bUfOVcLt-nQW9c6TaWeM0rKU3YzE6Yh3cuDe9ceOlBSH-w_B2HP9JpfK7T4-nYQj_WVhtk0iI9KP_idgFZ_oA"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<section class="field-guide ">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                                            <h2 class="title">
+                            Life is too short to let others' excuses slow down your change program                            </h2>
+                    
+                    
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>Every delay in your change program chips away at your reputation as a leader casting doubt on your ability to drive decisive action and deliver results.</p>
+<p>DOWNLOAD THIS LEADERSHIP FRAMEWORK<br>
+Master the art of strategic leadership: empower your team without sacrificing momentum. Learn the precise moments to delegate, coach or dive in and lead hands-on for unwavering stakeholder commitment and focus on company goals.</p>
+
+                                                    <h2 class="icon-list-title">This is for you if:</h2>
+                        
+                                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                        <ul class="custom-check-list">
+                                                                                            <li>Sense-of-urgency and being execution-focused are just buzz words among staff</li>
+                                                                                            <li>You would like to become more transparent and non-confrontational when reporting project risk</li>
+                                                                                            <li>Your project managers spend more time chasing and following-up than solving problems</li>
+                                                                                    </ul>
+                                    </div>
+                                                                    <div class="col-md-6">
+                                        <ul class="custom-check-list">
+                                                                                            <li>You would like to be more effective in defining roles and responsibilities to enhance sense-of-accountability</li>
+                                                                                            <li>Your team spends excessive time searching for and sharing information, know-how &amp; documents</li>
+                                                                                    </ul>
+                                    </div>
+                                                            </div>
+                                            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
