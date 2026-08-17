a2f40b48192be733311a58f683635b4bdd39faf1
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 10:33:47 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/talent-enhancement.php
@@ -0,0 +1,248 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+<section
+          class="resources-detail padding"
+          style="
+                    background-image: url(&quot;/wp-content/uploads/2025/07/Group-177.png&quot;);
+                    background-size: cover;
+                    background-repeat: no-repeat;
+          "
+>
+          <div class="container">
+                    <div class="row align-items-center">
+                              <div class="col-lg-8">
+                                        <div
+                                                  class="resource__wrap element-space-20"
+                                        >
+                                                  <h1 class="text-blue">
+                                                            Talent enhancement
+                                                            &amp; knowledge
+                                                            retention
+                                                  </h1>
+
+                                                  <style>
+                                                            .text-blue {
+                                                                      font-size: 24px;
+                                                            }
+                                                  </style>
+
+                                                  <h2 class="title">
+                                                            Not having control
+                                                            of your intellectual
+                                                            property archive is
+                                                            killing your ability
+                                                            to train, coach and
+                                                            retain your top
+                                                            performers!
+                                                  </h2>
+
+                                                  <div class="content">
+                                                            Avoid losing
+                                                            information from not
+                                                            being able to find
+                                                            it quickly in key
+                                                            moments due to poor
+                                                            data architecture.
+                                                            Say good-bye
+                                                            once-and-for-all to
+                                                            low retention rates
+                                                            resulting from
+                                                            ineffective training
+                                                            activities
+                                                  </div>
+                                        </div>
+                              </div>
+                              <div class="col-lg-4">
+                                        <div class="media">
+                                                  <img
+                                                            src="https://www.prestopdca.com/wp-content/uploads/2025/07/5-ways-to-revolutionise-knowledge-mgmt.webp"
+                                                            alt="ways to revolutionise knowledge"
+                                                  />
+                                        </div>
+                              </div>
+                    </div>
+          </div>
+</section>
+
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2788-o1" lang="en-US" dir="ltr" data-wpcf7-id="2788">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/talent-enhancement/#wpcf7-f2788-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2788" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2788-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA5S_6eUKcUNieb6j4gm_Mt9HAQhoRDZJbEih1O3QhNqv886ZD-0q8TmZSWecaa_V3CqSJ4vGZImdMxj-8EVNM5PKDOG8jYEIJe9ABOph5IG_iPYTjyvBBzWafvI3Czv3WkOQelSeSdKCSC2KKr2WPgMDHXiWg3zNE07_8naHpzDrntslM7a8jhGNIpQoVKmSLCbkBqd0aX9RMRpKQZ8LicjR6sUF7mDr2DBE8ritOxdRCmWaAHaeZmj0V3V4SyEylqESgT4O5AF7OBIIvOHUw9YxA3N1Ww-i94hq0AbXZQ_DBOjW9o4nh2odyWSFYZYNNSPZuKR8Xi4s4FqAofwceyd9uKcn02Dpp1PtStUYFTwUojCajwxradQthyvLGsxBZKZe68MC4N89ecUpvmdKvvWhBz_3Cy5UxqKJ3LpeugwtcPRK1t6p7IoBlByvu1nq9hpGD5c837iCV32eTPxlomQN5zy43BBLHondQccqho1_3ZVd0Yd_1frstzGUosJi8-xS4J5sUfa92dBnhObLmHBSifryyuc6kCGlOyi-7aRM8zxlk44h8EyPP1qtTo7CiF5YtGCXML-Q6pxcdhRijmmmTeqNTueWtzDp6Z-z_VeL7c-pZxWbsBz-zmySWlC2MGqvZcWOfXLHu6J_WIenkB4nBj08_kRk45Exbl6r2phOSaOLj49BO91jRVzfkOwGXi3yd8fMBqz1ZIWwnPIrujYz3ub6aMinte3Mvl5SC0l8fFWlJju8JtHmRkeAJM-CUtFwxPw82W1XZMTzbWvwkBhJhH8pQX6n7S5Hx2uIgLfXhhSH1EwGC7Bgf6bN1aYwy-zB42NHrJXc-Gvvn2h07luvezH2ivnmmcrqhO0JWPVKXexDc-eseYq8clBMG5BOvs7L4G-s0xsqQmOVaYotZ8fmVytb5lOGio1CUeL58xolQ6L_Sf8NImz7rACSGjd2j0qENK22lI_zUa7GHfmviIXLyP3YsCPIBX_c6qbNNFwx7VQPHPJgXrHFlRedD9ySUay09y8_mMr8tMBpUNLw8c56OoLa70jCQcftkdmcGFWMQFJfKbsHO8ofVxaDx9PQsAPMTA2ixqWWldcfpDKwjDo9GZoq3NuQfmHpL71Jjm6jHIzbXb1-vTjJp1hyNaFE5noXKx0WZCMyfKxTP_1o3wnybeixUTPAInJYg3QbnFZ5z5ybSEPAL9vLTQK9BJ4ZXQChLWTwyGRFACfoieZ7Ir7ciR-_i3BLASwHo8Ne3YSN0VCea8Vu1ZPKwn0MEMcyiSa9XCi1aNP_V4AhBKogCugeWOroRgiCxXECZU3D_TfLGsMJZTD5JgpNDSWHeZ-cK_3AtsO-fZTBYB2b9InZS9aXJw6ORHXIeDerxi3L4xPnbQy7RzR7VwrRfpz0inUkA4uxiAeN_B2JCasCYumZG_4mfI3O-QOZSCvF740PMxWN8oONYp6HVg2A-Auqu9hhdEOT37ppMOVwsbFHrgzoZg--Yqcf4tg-oDOt5VTezNSt_Y7um--7zBBbUQJZ27xbL8_thM3nSPNDXt0wdcsrxB9E9k-k-uCME_U5oVMzbU2jTXtv_P65uN9PZQmgstQ8V2yOAVRwsAY25rQevQvijoPlrYEvSuXc2bKxH4Wa6oLA-KArtETmVpRietY27pH15SrKEDe621Q5RDaz-myQfSkwh48MmW96YtGXr2FBH1su-3SDJ7DYv4yV4gnfCiubhnUHoaicx8QDByG0zrmoYOwsbvFQ7160W0cCM9C5mg8r5sDYgk6VQzZ7dghOUP3HXk7AKB1_JirXroVYnCxIekhMABDBt6a0c0bZIz0f3_X24fGOc33lhknJp18Q9Yhwqkhw4s9uXTFLEGWXi7OR6O8CV9RTtu6zj5t9dzK4tQTqr-VQq7jq6OnZ6wEl2QXnQQ1vNv3PYY5wpGnOrRzIWqImRIjCVhpbwCC-UttAwV5cgptvVaqnqMGTxFBzHm8iNNS7FR5D-JcPkDtxB7l37PYm9obRl6fsdaZAQ0yOQKzSbe9Y51chi0YuYIQkGd2Rdgljh7znlQLzH3e2FTcpIeAnvmj1QEhkqOhriuq1HtHxbR25OD-NytqZ3Xs51GGw8ljOb2YhrwqPvRG9NdHIA6qVjTeFKTyFfqDl2DqfA9P6uk-V2tDzKl7t90O2p63-1sCEfMugYfvchpIAKMZPN_bEORY6b0zcuNDsFNctuQyLO9JhmX3BBfozdpJaRan1qyWzZaYka3Wl50mR3dgpC57K55oDg"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="field-guide">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                    <h2 class="title">Are you doing these 5 things?</h2>
+
+                    <h4>Help HR join forces with Business Operations</h4>
+
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>
+                            Say goodbye to ineffective training activities and the infamous ‘forgetting curve’ which
+                            eats away at your ability to execute. Use PRESTO PDCA talent management software to
+                            transform team leaders into coaches equipped with the tools they need to offer daily
+                            incremental constructive feedback to their staff.
+                        </p>
+                        <h4>DOWNLOAD THIS 5-STEP ROADMAP</h4>
+                        <p>
+                            Discover the 5 key focus areas of digital transformation success in knowledge management
+                            &amp; talent retainment using PRESTO P-D-C-A! This job aid unveils the top 5 essential areas
+                            of talent management that must be your top priority to achieve a seamless transformation.
+                            Learn how PRESTO P-D-C-A can support and empower your organisation to thrive in today’s
+                            ever-changing digital landscape. Don’t wait any longer to unlock the full potential of your
+                            business – get started with PRESTO P-D-C-A today!
+                        </p>
+
+                        <h2 class="icon-list-title">This is for you if:</h2>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>
+                                        You are still using spreadsheets to track &amp; report staff skills &amp;
+                                        process competencies
+                                    </li>
+                                    <li>You would like team leaders to become more coaching-centric</li>
+                                    <li>
+                                        You are spending excessive time chasing colleagues, vendors &amp; clients for
+                                        feedback
+                                    </li>
+                                    <li>
+                                        Your company uses the R-A-C-I accountability matrix and would like to digitise
+                                        its use
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>Sense of ownership &amp; sense of urgency among staff is mediocre (at best)</li>
+                                    <li>
+                                        You wish your archive of knowledge and intellectual property was more visible
+                                        and accessible to who needs it, when they need it
+                                    </li>
+                                    <li>
+                                        Your staff has difficulty aligning their every-day work to your company's
+                                        strategic goals (and vice-versa)
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
