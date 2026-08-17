03b2051d338745e33a1da18a1fc0c37c4ae98d3b
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 11:31:31 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/iso-9001-certification.php
@@ -0,0 +1,221 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+
+
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">ISO 9001 Certification&ZeroWidthSpace;</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">ISO 9001 Jump Start Program</h2>
+                    
+                                            <div class="content">Accelerate Your ISO 9001 Certification Without Hiring Costly Consultants We want you!
+
+</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/iso9001.png" alt="ISO 9001 Certification&ZeroWidthSpace;">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2792-o1" lang="en-US" dir="ltr" data-wpcf7-id="2792">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/iso-9001-certification/#wpcf7-f2792-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2792" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2792-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA7k44ewiwTlnGbvpMoe02G2wvGV1LLtot2HVQhk3ijQ7K0dEtEr-P3ZV-JO_WdmvmGNPCT7yBeOxKwWgDNSmF-FDoPxNPcoebCAiXt5kwPKmhZ6ZcCXQDewVET6CoYY9saLzsrWLBieQCLIy_sJCa03ibWVmszTDlPYeUQMMPgJWrqqUhCfLXOKBgrtZigFQHFYYWkkLpVnBffFnDLHQY5r8x7y4OCDd0vL8N34RCtjoJGKe6L6xfc4LpIuMnz3N9po78YfnW8_R2KqluoYq7GYiduR4M1EptcI2Q9HGW6FFVzERVbVzFN_8ghBjCBwBnSmhtOzNBIr4aaWSavH5-pjondeNgHjVGsZa2ZLQr7kUZ-8X3uuepfwYJAChTPDqY3CRpNB_HxS6liKPJE2aEvNDZilZ6LnjUdm9It98p59YMB_3lS2IQRVuZno_lpggFpSU4l3kIxPkZ12gYPsuXdy0d6TFN-C2OxH8XJ8IzCqvz5_07MJPnn9IvddMjYDbRwB1TwjAK5BjZvgDx4TVOc1oZ5gCfzK9SKbRJ6eTQ1oekn7LErNX6o9qVTyftFGfRfdauTmChThf-sprPhFKa9kwFoqLYqan5Sr1Bpli5r-JwGKKHphZkua_lRS7BUZ6g53HADaFVI9y6JpuW9_ntAVHfBceJuwqWh581pyFLE_lHdcH5sMXZeXBwdd-NIqV7KHojeZZ7TvqpKpITBu9WC4cOlOIEVe-IM62IRzxIn_GPzYkJxr8vletUUY7vq24LhPAy_ID4qOMF6I2KmKGUrvNSh5NCxYfFSF0POfLSeN5h7tHJCy41NegGw1QeJ61TljQB50m59C_Z9xPzFmGPsAbgpFDDy70Iz5R8sAEMSoQW6gjaUcMJZvuAygpxfOaYHMUBqV--UsnZ8GWPbIdWeoFl_aF1KMiWKI4_t163_C90g5j9XNVbk4eG12nVDlY4UwcnG4UrMX-9VkSzDLmn3KeHmaqi1_XiwpFPOYAvZBe6WDjaaJZ7sEN0GK3TIIZVr1V_von-fomsynpXQMsikbYBHdvwGL_HtgBBpW5Poujs0tLzgMofA1IxS8XZLxbEsPGkcwiaAFCFzAHtKuOGXPML5z1lo1merL5qTsHt4V9EFkUIPz4YW1PAVL8kSevOfDwjq6XuLuOpmLozd_1VCbVik9BMakK_WwVlkLKL4TlEFvHD89Y1e0ULI-Mlxcp8wSSjnfD-A4xv9BukgHdA9e690gNLYj2s35N9PJG5DrLtp8lZt4AfBkAgzl_P2wc11SNAONRcPpYx-F-rkoJIadTwogvmQZIAK4zXD3b8jIbT3GimLdhz8cnog2N73h7lsl1ExV4JEuG9A-zqxeDsQKAWD_7N6zVZBLcT6QcpbbC-8lKXs_4BOUTOqlJ9ufWmxRTjWYxEJVnm3xYId4SIWtzd5yOD6plwZkEujjD09FmF7kk_aOLZl2NJzSThdhhfLRhvIjY08eD5e_8N4XEe6WM44W24yjULkJSE3DRWzP_I4PxH6n7vV1sSmPdu1fm3OIBWSIdpXeFYYVw3Olv-6X6MVkiO5c38EkjmYYUo7u_72dnmLqzFK1f6I_PIQB6GC46Y_OpwVH8kcinSC1NR5Ro6vdTOi9-x519liP0Tir6eL33-TYXACALR0s8W9WGTNL0cm5AR2eamlpDbaF8wgvCbwHK4vJ-4n7Kb2ycTC71g0GKNuXQxdoncl5UI7ggJER6DOkSpusoHaPDXb28lPm1M6Yj0cFYjmp3hfXoPy-vl6nIuU3wkL78CaemwfSzJ08LrHEqeLDEHN3-Ogv218rlqi9Pf0DlI2cA_tVlSSTLA9fQFGt6EW-8_O90Z4CfO1vP4Xbny6bmUpBZElj9vEkwpsmdfyo35kD1grjoVp8TIFhKDnSAdBzEf7Vt3NMKkvqMEMBMfMlkzKcp7aMZXP77YD5X5y_cFzqQSSjrBU0_ghBD_v4ndpTBLBxPq62YrLBweLsKIQxqjf-sK5m3Hcu9eA34ok3slyBARDZq6Z07BJnFAyF2s97cVS3qiNV4D8SJuqfFNsVISsvLmMsawvpokysimvOXlwmUdQec7cloD9Jpk4qf6biTUrID_TY-9fodMwYofXTMqeZXxp0Jfinaz09liROA2bB_GfIUXIt4wsB46k0s_hOuuyyptfhapslgRPVu4-hCSPctj2Ll2wsb1VxrDmD7y4CJb-NftrpyLPtHtpLgR0ZGberaCv3RcXVKck6"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<section class="field-guide">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                    <h2 class="title">Empower Your Business with a Proven Quality Management Framework</h2>
+
+                    <h4>Special Offer</h4>
+
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>
+                            After years of helping organizations streamline their processes, our ISO 9001 Jump Start
+                            Program is now available in an online subscription model, offering small/mid-size businesses
+                            and leaders a fast-track approach to ISO 9001 compliance and certification.
+                        </p>
+                        <p>
+                            Complete with self-service online training and Presto P-D-C-A software integration, our Jump
+                            Start Program lays out an entire ISO 9001 template-giving you a step-by-step framework to
+                            capture all business processes and support documentation in one place. Keep everything
+                            updated in real time, ensure team-widevisibility, and skip the high fees of hiring an
+                            expensive consulting firm.
+                        </p>
+                        <p>
+                            With the ISO 9001 Jump Start Program, you gain immediate access to a ready-to-use framework
+                            covering every required element of the standard, supported by our user-friendly Presto
+                            P-D-C-A platform. This ensures your entire team can seamlessly capture processes, track
+                            updates, and collaborate-no technical expertise required. You’ll also receive hands-on
+                            coaching to maintain compliance and effectiveness as your business scales, all while saving
+                            money by bypassing expensive consulting fees. By consolidating everything into one
+                            accessible platform, you boost operational visibility, reduce administrative overhead, and
+                            foster a culture of quality. Whether you’re a small or mid-size enterprise, this program
+                            offers a practical, easy-to-implement solution focused on continuous improvement instead of
+                            juggling spreadsheets and outdated documents.
+                        </p>
+                        <h5>Ready to fast-track your ISO 9001 certification without expensive consulting firms?</h5>
+
+                        <h2 class="icon-list-title">This is for you if:</h2>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>You've postponed ISO 9001 certification because of high consulting costs</li>
+                                    <li>You've postponed ISO 9001 certification because you don't have the time</li>
+                                    <li>
+                                        You struggle to keep your business processes consistently documented and up to
+                                        date
+                                    </li>
+                                    <li>
+                                        Your team lacks a clear sense of ownership when it comes to quality management
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>
+                                        You need real-time visibility into how tasks are performed and where
+                                        improvements are needed
+                                    </li>
+                                    <li>
+                                        You want a practical, easy-to-implement solution that scales as your company
+                                        grows
+                                    </li>
+                                    <li>
+                                        You're tired of juggling spreadsheets, emails, and outdated documents to manage
+                                        compliance
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
