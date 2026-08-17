dc43a898be93f69db64d02decdc2e2df0f07b3bb
SquareAero <104221730+SquareAero@users.noreply.github.com>
2026-08-17 11:03:14 +0530

    Add files via upload

---
+++ Learn/Resources/single-resources/strategic-planning.php
@@ -0,0 +1,191 @@
+<?php
+include __DIR__ . '/../../../header.php';
+?>
+<section class="resources-detail padding " style="background-image: url('https://www.prestopdca.com/wp-content/uploads/2025/07/Group-177.png'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8">
+                <div class="resource__wrap element-space-20">
+                                            <h1 class="text-blue">Strategy Planning with OKRs</h1>
+
+                        <style> .text-blue{font-size:24px;}</style>
+                    
+                                            <h2 class="title">Strategy Step-by-Step Execution</h2>
+                    
+                                            <div class="content">A do-it-yourself field guide to model, deploy and sustain objectives and their key results using PRESTO P-D-C-A strategy management software
+
+</div>
+                                    </div>
+            </div>
+            <div class="col-lg-4">
+                                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/07/OKR_field_guide.webp" alt="strategic planning execution">
+                    </div>
+                            </div>
+        </div>
+    </div>
+</section>
+
+<div
+    class="container bg-light-blue border-radius-35 paddingformcontainer mt-5 mb-5 pt-1 pb-1 new_hubpost_form_resorses"
+>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="title text-center">Download Your Free Resource</h4>
+
+            <div class="wpcf7 js" id="wpcf7-f2798-o1" lang="en-US" dir="ltr" data-wpcf7-id="2798">
+                <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
+                </div>
+                <form
+                    action="/resources/strategic-planning/#wpcf7-f2798-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                    data-hs-cf-bound="true"
+                >
+                    <fieldset class="hidden-fields-container">
+                        <input type="hidden" name="_wpcf7" value="2798" /><input
+                            type="hidden"
+                            name="_wpcf7_version"
+                            value="6.1"
+                        /><input type="hidden" name="_wpcf7_locale" value="en_US" /><input
+                            type="hidden"
+                            name="_wpcf7_unit_tag"
+                            value="wpcf7-f2798-o1"
+                        /><input type="hidden" name="_wpcf7_container_post" value="0" /><input
+                            type="hidden"
+                            name="_wpcf7_posted_data_hash"
+                            value=""
+                        /><input
+                            type="hidden"
+                            name="_wpcf7_recaptcha_response"
+                            value="0cAFcWeA4myeQvzVSkFGBGNt8hbf3U_qBIMcarpRJqlxUGFU4HD3fJuWI9U0pDIEF9eS5pHhzCQGRVOoT91qnENdby-qTsY_5TU3xW3txnSSKzPZj1rlGvuke0HM2ZPtTy-P5wzxCuHniGX3OYANfz3DBqzITF9HzaLrW-gi-PGU4r-Ugrq5v6ruYJ6rCx0tDHsD7oFVfXtqVLFDnsZN42JbZKa9lvxqS5dSLRWCtFvOcec_SUjXU3-fq6BoXo172-XVHLwnd3Nag5LGTWs-9pFkv2mWO5LQRwm8fgL0d9NJHyyDglqSLQKHXRCS5ZIo_9PmGJhRHp8UblPmnleIp86rnOjy1Fn-P6uebKBRPPIN86vV7x2otQeZ_IpfIeFJ6BfF7xxTZPeE6nM1hcfGHeNbfWuH05YzPz_gAqq372Afk_bRtb3eQlI7OAjYDBprALoeuEDTBhVy3JyFV1lVBRLc9wS3iwULEN8fC3hav7kwybV76FMEkwNonJjLCkYtsqyfcuXSUHlU5fiLJlY5fbQYwQ_BNuFgA3LhXa4zF695o6IhZ61JBAtjUjKM_mgNTAZVsLAJ4533X4-9w9Lv6Fjnlx4QpwxxH6EGkXSHfdO46RS0aPWtZ1nMvl_oO2LIcsVu6dvsCoaSeMBmgwnrQn6Jlq0EbosGxqBYob-r9wiA-qwVoQmtzYF0n9ypesUMO7azdHIvSwvbT_Biy5BNw4jkzDBlSZDdLEC4S-IIoRcmE6p5ozjJskHpIQ9BaWXG0epRMUuWHaSAaBLdhhbZqRd36RF_CB_CMDjgDVQ6I_adBgFmvXjc-QdlZfwhXZyItccBO-Hfl9k2vzEikhyaFh6ZgypSC7yxTu9GTZp1iAn2Oz-sJpwrzF7lrTkWQNxv1eXGFQ8wZMvr295MaAZF17hdHVgD6UxB9UyInW14Dplk4ceAxu7wvQTWkBhrSHOANJA_tpnYfkl4K0H7LcImOHSgZ6sJrSm2KPmr11bgzYjq6SYSfzJNHpiTH0iF009DZ8uyhvoy5QXARRrfImUnpq3uX3XyyGePPJc05VeQ3Z1Rwel40RXQ_FutgiD9YBlIYGvrT0QMTGdzobNERM59T0cbfEzFjoBtx-X3YOlao-D6ewUU25R5GCTPqkDo6lXttrI6vMyP3ApBg_ytAugjcZXFbc4Q_m3bK0o1kRxGz1pXewjuW1WAEgF2yLFIIySKlxkA4j6E74P77IqTBYDpcoJvqsTUWRGcLkhuQh8UpUt1ZlkCjqzvVhkX4OHkL0OSA0RqE67rG8rgukyhaBkoEW3PQ9Xi9nY1JIObgygm03wZz3QN3I7Jx0GiZeAs5jWbJCaVuMJJI5JzrIGZBC6zXOdSi_JU1Uzo0UgKmRusGkmD9bAHeJaD2g8n3hkeeyZ2yW9hVMvHm__nxqDpT8TEnr-cpoPtX-ouN6zA0L4V5RXxo_-x5QNf_LcuunD7Xxxcl5cCbwo3soYxKIViOE19OznVFaD-b1Xu4koo6-9-eo24CQymZtKUiGZrDwqiJsCLpeyaZm0IbeOXsFW1dwTEWn0lu_Uh2GB_bM3OZDOtZ3y3t0mzNfn-8H1Qhwgj4xXa3grrIb5qexnsu2DltTsNvmzdI_lOucVVNe4lgtGxkhhZBlcqgKm1oWzatajs3rVWwMbESL_pOir6RkH3APu1nXEp1JPmGqC28_BfKqQCj4KVYxvNRotBZg5KtQj1N6_1_qpeMlbWS__nABfzx30Wf81P__hXChZqAXuqZo2NadyLbN1hej0v48Qoij5Lf8VkPotvrMidWCBwVGilieqeYfiUvMuJoSdCbRDe3pV1gvYyC3sn2VjXQsQD0g3_wAvxCoPrECfcj0nRAKGIZK-YgVaKvTZfTovw-ffot1pnxM9W2P9YAEcIfgoo-IQxB09wpcai2bMCCnFE2XIKQPGEN9da42tzmAdloP6Rla8U-V8nl4vEvhPFGXy6vBCTIsCbHHmXb3Wm_GNvwRMhzAarN8GOI3WeQqGL8O68HVtBrbIzPwCoc8T6cxkPlXOFHSIubz1CEjX8QsCvLnN3UQt_85sYTtwZKS8V3vARPq5Ane4FbpXzM3IPYtm8QmVw73hABImlM24CaTcDrva1UF4iRuMIXNa1SKgJhkauVswHnSPQEmr5oQmuxjDrFweds3JXmh4BCQJEtrIKSfyqktiBrdGrA0D1iEzTct5SN1gCGwiBxXvBLQYTqHwcx5rZpcdvbbvF7SGrwvpH7X"
+                        />
+                    </fieldset>
+                    <p>
+                        <label>
+                            Your name<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-name"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                    autocomplete="name"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="text"
+                                    name="your-name"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email<br />
+                            <span class="wpcf7-form-control-wrap" data-name="your-email"
+                                ><input
+                                    size="40"
+                                    maxlength="400"
+                                    class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                    autocomplete="email"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                    value=""
+                                    type="email"
+                                    name="your-email"
+                            /></span>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Submit" /><span
+                            class="wpcf7-spinner"
+                        ></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="field-guide">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-lg-1">
+                <div class="section-title element-space-20 text-center">
+                    <h2 class="title">FREE FIELD GUIDE</h2>
+
+                    <h4>Making OKRs work for you</h4>
+
+                    <div class="resource-bottom-desc element-space-20">
+                        <p>
+                            In this field guide you will learn our tried-and-true road map to launching a strategy
+                            deployment program which captures, reports and sustains the objectives and key results
+                            linked to company breakthrough goals (aka OKRs).
+                        </p>
+                        <p>
+                            By investing in 3D printing prototype technology, the Dupont Board of Directors plans to
+                            revolutionise their design and go-to-market processes by widening their product line while
+                            increasing quality and making their company more eco-friendly and cost effective.
+                        </p>
+                        <p>
+                            This field guide walks you through the step-by-step process of creating a cluster of
+                            cross-departmental OKRs that support the Dupont team leaders to enable each to approach
+                            their management team’s long-term vision with measurable, achievable short term incremental
+                            key results.
+                        </p>
+
+                        <h2 class="icon-list-title">This field guide thoroughly explains how to use OKRs to:</h2>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>Practically apply OKRs into the daily management of business operations</li>
+                                    <li>Cascade board room strategic initiatives to line managers</li>
+                                    <li>Assign key results to staff member clusters and virtual teams</li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="custom-check-list">
+                                    <li>Monitor &amp; sustain OKRs using a dashboard</li>
+                                    <li>Use RACI and the Balanced Scorecard logic to enhance your OKR program</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="get-started bg-light-gray position-relative ">
+    <div class="container">
+        <div class="row row-gap-5 align-items-center">
+            <div class="col-md-7 mb-5 mb-md-0">
+                <div class="started__content">
+                    <h2>Get Started in Seconds</h2>
+                    <div class="btn-wrap justify-content-start">
+                        <a href="https://www.prestopdca.com/contact-us/" class="btn btn-fill btn-icon icon-white">Book a Demo</a>
+                        <a href="https://www.prestopdca.com/pricing/" class="btn btn-icon icon-black">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="started__img">
+                    <div class="media">
+                        <img src="https://www.prestopdca.com/wp-content/uploads/2025/06/get-started.webp" alt="Get Started Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include __DIR__ . '/../../../footer.php';
+?>
